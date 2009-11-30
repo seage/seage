3a4bff12a020bd44474f970e73a517584a668311
Richard Malek <rick@rickq.net>
2009-11-30 11:48:19 +0000

    Minor changes.
    git-svn-id: https://seage.svn.sourceforge.net/svnroot/seage/trunk@27 69285ed8-183e-4c31-bb72-992939805efb

--- metaheuristics/src/org/seage/metaheuristic/sannealing/SimulatedAnnealing.java
+++ metaheuristics/src/org/seage/metaheuristic/sannealing/SimulatedAnnealing.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Richard Malek and SEAGE contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://seage.sourceforge.net/license/cpl-v10.html
+ *
+ * Contributors:
+ *     Jan Zmatlik
+ *     - Initial implementation
+ */
 package org.seage.metaheuristic.sannealing;
 
 
@@ -203,7 +214,7 @@ public class SimulatedAnnealing implements ISimulatedAnnealing
           _listenerProvider.fireNewBestSolutionFound();
       }
 
-      if( anneal( modifiedObjectiveValue - _currentSolution.getObjectiveValue() ) )
+      while(!anneal( modifiedObjectiveValue - _currentSolution.getObjectiveValue() ) )
       {
         _currentSolution = modifiedSolution;
           System.out.println("OOOOOOOOOKKKKKKKKKKKKKKKK");
