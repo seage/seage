ab832c83c8c60f8ef545618b5b8ab3fd03c130b9
Martin Zaloga <m.z.work@seznam.cz>
2009-12-20 00:21:52 +0000

    Small edits.
    git-svn-id: https://seage.svn.sourceforge.net/svnroot/seage/trunk@51 69285ed8-183e-4c31-bb72-992939805efb

--- seage.metaheuristics/src/main/java/org/seage/metaheuristic/hillclimber/HillClimber.java
+++ seage.metaheuristics/src/main/java/org/seage/metaheuristic/hillclimber/HillClimber.java
@@ -2,64 +2,53 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.seage.metaheuristic.hillclimber;
 
 /**
  *
  * @author rick
  */
-public class HillClimber implements IHillClimber
-{
+public class HillClimber implements IHillClimber {
+
     private int _numIter;
     private Solution _currentSolution;
-
     private IMoveManager _moveManager;
     private IObjectiveFunction _objectiveFunction;
 
-    public HillClimber(IObjectiveFunction objectiveFunction, IMoveManager moveManager)
-    {
-        _moveManager = moveManager;
-        _objectiveFunction = objectiveFunction;
+    public HillClimber(IObjectiveFunction objectiveFunction, IMoveManager moveManager) {
+        this._moveManager = moveManager;
+        this._objectiveFunction = objectiveFunction;
     }
 
     //TODO: A - review the code bellow
-    public void startSearching(Solution solution)
-    {
-        _currentSolution = solution;
+    public void startSearching(Solution solution) {
+        this._currentSolution = solution;
 
         int iter = 0;
-        while(iter < _numIter)
-        {
-            IMove[] moves = _moveManager.getAllMoves(_currentSolution);
+        while (iter < this._numIter) {
+            IMove[] moves = this._moveManager.getAllMoves(this._currentSolution);
 
             IMove best = null;
-            for(IMove m : moves)
-            {
-                double bestVal = Double.MAX_VALUE;
+            double bestVal = Double.MAX_VALUE;
+            for (IMove m : moves) {
+                double val = this._objectiveFunction.evaluateMove(this._currentSolution, m);
 
-                double val = _objectiveFunction.evaluateMove(_currentSolution, m);
-
-                if(val < bestVal)
+                if (val < bestVal) {
                     best = m;
+                    bestVal = val;
+                }
             }
 
-            _currentSolution = best.apply(_currentSolution);
+            this._currentSolution = best.apply(this._currentSolution);
             iter++;
         }
     }
 
     public void setIterationCount(int count) {
-        _numIter = count;
+        this._numIter = count;
     }
 
-
-
-    public Solution getBestSolution()
-    {
-        return _currentSolution;
+    public Solution getBestSolution() {
+        return this._currentSolution;
     }
-
-
-
 }
