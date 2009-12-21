61b1b8e61ca173249efd8080316b7c7a95b798ac
Richard Malek <rick@rickq.net>
2009-12-21 10:56:07 +0000

    HillClimber - added empty classes (Random- and Greedy- solutions)
    git-svn-id: https://seage.svn.sourceforge.net/svnroot/seage/trunk@65 69285ed8-183e-4c31-bb72-992939805efb

---
+++ seage.problems/tsp/src/main/java/org/seage/problem/tsp/hillclimber/TspRandomSolution.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.seage.problem.tsp.hillclimber;
+
+/**
+ *
+ * @author rick
+ */
+public class TspRandomSolution {
+
+}
