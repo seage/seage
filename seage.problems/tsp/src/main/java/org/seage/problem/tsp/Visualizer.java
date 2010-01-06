a347e8623c4c587aad9c56722d3b2cdbec41fe98
Jan Zmatlik <zmatlja1@fel.cvut.cz>
2010-01-06 23:40:40 +0000

    Inclusion TourProvider to TspSA and implements Random and Sorted algorithms to TourProvider. Majority changes in Vizualizer - create new solution.
    git-svn-id: https://seage.svn.sourceforge.net/svnroot/seage/trunk@86 69285ed8-183e-4c31-bb72-992939805efb

--- seage.problems/tsp/src/main/java/org/seage/problem/tsp/Visualizer.java
+++ seage.problems/tsp/src/main/java/org/seage/problem/tsp/Visualizer.java
@@ -14,24 +14,14 @@
 package org.seage.problem.tsp;
 
 import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.NoninvertibleTransformException;
 import java.awt.image.BufferedImage;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.util.ArrayList;
 import javax.imageio.ImageIO;
-import javax.swing.BorderFactory;
-import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-import org.jgraph.JGraph;
-import org.jgraph.graph.DefaultEdge;
-import org.jgraph.graph.DefaultGraphCell;
-import org.jgraph.graph.DefaultGraphModel;
-import org.jgraph.graph.GraphConstants;
-import org.jgraph.graph.GraphModel;
 
 
 /**
@@ -54,26 +44,11 @@ public class Visualizer
     }
     // </editor-fold>
 
-    private JFrame frame;
-    private GraphModel model;
-    private JGraph graph;
-
-    ArrayList<DefaultGraphCell> cells = new ArrayList<DefaultGraphCell>();
-
-    public void createGraph(City[] cities, Integer[] tour, String path, int width, int height)
+    public void createGraph(City[] cities, Integer[] tour, String path, int width, int height) throws NoninvertibleTransformException
     {
-         // Create the model to use.
-        model = new DefaultGraphModel();
-        graph = new JGraph( model );
-        graph.setAntiAliased( true );
-        graph.setEditable( false );
-        
-        // Show the graph
-        frame = new JFrame("Travelling salesman problem visualiser");
-        frame.setPreferredSize( new Dimension( (int)(width*1.5) , (int)(height*1.5) ) );
-        frame.getContentPane().add( new JScrollPane( graph ) );
-        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-        
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB );
+
+        Graphics2D graphics = (Graphics2D)image.getGraphics();
 
         // getting current width ang height
         double minX=Double.MAX_VALUE, minY=Double.MAX_VALUE;
@@ -85,64 +60,65 @@ public class Visualizer
             if(cities[tour[i]].Y < minY) minY = cities[tour[i]].Y;
             if(cities[tour[i]].Y > maxY) maxY = cities[tour[i]].Y;
         }
-        double currWidth = maxX-minX;
-        double currHeight = maxY-minY;
 
-        DefaultGraphCell firstNode = createCell(tour[0].toString(), width*cities[tour[0]].X/currWidth, width*cities[tour[0]].Y/currHeight);
-        cells.add(firstNode);
+        double currWidth = maxX - minX;
+        double currHeight = maxY - minY;
 
-        DefaultGraphCell currentNode = firstNode;
-        DefaultGraphCell lastNode = firstNode;
-        for (int i = 1; i < tour.length; i++)
-        {            
-            lastNode = createCell(tour[i].toString(), width*cities[tour[i]].X/currWidth, height*cities[tour[i]].Y/currHeight);
-            DefaultGraphCell currentLink = createEdge(currentNode, lastNode);
-            cells.add(lastNode);
-            cells.add(currentLink);
-            currentNode = lastNode;
+        AffineTransform transform = graphics.getTransform();
+
+        // change the start coordinates to left down corner
+        transform.translate(0, height);
+
+        // rotate to fourth quadrant (180 degrees about x-axis)
+        // quadrant * Math.PI / 2.0
+        transform.quadrantRotate(3);
+        graphics.setTransform(transform);
+
+        graphics.setColor(Color.black);
+        graphics.setBackground(Color.white);
+
+        // TODO: A - Resolve points over bounds
+        // draw first city
+        graphics.fillRect(
+                    (int)(width*cities[tour[0]].X / currWidth)-1,
+                    (int)(height*cities[tour[0]].Y / currHeight)-1, 3 , 3 );
+
+        int i = 1;
+        for( ; i < cities.length; i++)
+        {
+            graphics.fillRect(
+                    (int)(width*cities[tour[i]].X / currWidth)-1,
+                    (int)(height*cities[tour[i]].Y / currHeight)-1, 3 , 3 );
+            
+            graphics.drawLine (
+                        (int)(width*cities[tour[i]].X / currWidth),
+                        (int)(height*cities[tour[i]].Y / currHeight),
+                        (int)(width*cities[tour[i - 1]].X / currWidth),
+                        (int)(height*cities[tour[i - 1]].Y / currHeight)
+                    );
         }
 
-        DefaultGraphCell lastLink = createEdge(firstNode, lastNode);
-        cells.add(lastLink);
-        
-        frame.pack();
-        frame.setVisible(true);
-        graph.getGraphLayoutCache().insert(cells.toArray());
+        // draw line between first and end city
+        graphics.drawLine (
+                        (int)(width*cities[tour[0]].X / currWidth),
+                        (int)(height*cities[tour[0]].Y / currHeight),
+                        (int)(width*cities[tour[i - 1]].X / currWidth),
+                        (int)(height*cities[tour[i - 1]].Y / currHeight)
+                    );
+
         try
         {
             File f = new File(path);
             if(path.contains("/"))
                 new File(f.getParent()).mkdirs();
             BufferedOutputStream out = new BufferedOutputStream( new FileOutputStream( f ) );
-            BufferedImage image = graph.getImage(graph.getBackground(), 0);
+
             ImageIO.write(image, "png", out);
             out.close();
         } catch (Exception e) {
             System.out.println("Chyba");
             e.printStackTrace();
         }
-
     }
-
-
-    private  DefaultGraphCell createCell(String name, double x, double y) {
-        DefaultGraphCell cell = new DefaultGraphCell(name);
-        GraphConstants.setBounds(cell.getAttributes(), new Rectangle2D.Double(x, y, 2, 2));
-        GraphConstants.setBorder(cell.getAttributes(), BorderFactory.createLineBorder(Color.BLACK));
-        GraphConstants.setSelectable(cell.getAttributes(), false);
-        cell.addPort(new Point2D.Double(0, 0));
-        
-        return cell;
-    }
-
-    private DefaultGraphCell createEdge(DefaultGraphCell source, DefaultGraphCell target) {
-        DefaultEdge edge = new DefaultEdge();
-        source.addPort();
-        edge.setSource(source.getChildAt(source.getChildCount() -1));
-        target.addPort();
-        edge.setTarget(target.getChildAt(target.getChildCount() -1));
-        GraphConstants.setLabelAlongEdge(edge.getAttributes(), false);
-        GraphConstants.setSelectable(edge.getAttributes(), false);
-        return edge;
-    }
+    
 }
--- seage.problems/tsp/src/main/java/org/seage/problem/tsp/sannealing/TspRandomSolution.java
+++ seage.problems/tsp/src/main/java/org/seage/problem/tsp/sannealing/TspRandomSolution.java
@@ -8,15 +8,11 @@
  * Contributors:
  *     Jan Zmatlik
  *     - Initial implementation
- *     Richard Malek
- *     - Random algorithm
  */
 package org.seage.problem.tsp.sannealing;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 import org.seage.problem.tsp.City;
+import org.seage.problem.tsp.TourProvider;
 
 /**
  *
@@ -24,32 +20,10 @@ import org.seage.problem.tsp.City;
  */
 public class TspRandomSolution extends TspSolution {
 
-    private Random _random = new Random();
-
     public TspRandomSolution(City[] cities)
     {
         super( cities );
-        initRandomTour();
+        _tour = TourProvider.createRandomTour( cities );
     }
 
-    private void initRandomTour()
-    {
-        List<Integer> listTour = new ArrayList();
-        for (int i = 0; i < _cities.length; i++) {
-            listTour.add(i);
-        }
-
-        List<Integer> randTour = new ArrayList();
-        Integer pom = null;
-        for (int i = 0; i < _cities.length; i++) {
-            pom = listTour.get(_random.nextInt(listTour.size()));
-            randTour.add(pom);
-            listTour.remove(pom);
-        }
-
-        _tour = new Integer[_cities.length];
-        for (int i = 0; i < _cities.length; i++) {
-            _tour[i] = randTour.get(i);
-        }
-    }
 }
