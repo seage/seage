6408be0694b4ba7f982930ec4d996e6a72de8c2c
Martin Zaloga <m.z.work@seznam.cz>
2010-01-28 23:07:51 +0000

    separate between restart algorithm from a problem-dependent parts
    git-svn-id: https://seage.svn.sourceforge.net/svnroot/seage/trunk@121 69285ed8-183e-4c31-bb72-992939805efb

---
+++ seage.metaheuristics/src/main/java/org/seage/metaheuristic/hillclimber/ISolutionGenerator.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.seage.metaheuristic.hillclimber;
+
+/**
+ *
+ * @author Zagy
+ */
+public interface ISolutionGenerator {
+    Solution generateSolution(String clasic, String switcher);
+}
