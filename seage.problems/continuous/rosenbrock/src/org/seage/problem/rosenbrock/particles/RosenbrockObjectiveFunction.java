3096034f91d3a291dbbf5662a872bf95f2a6f523
Jan Zmatlik <zmatlja1@fel.cvut.cz>
2010-02-16 17:55:19 +0000

    Solving problem with Rosenbrock and PSO
    git-svn-id: https://seage.svn.sourceforge.net/svnroot/seage/trunk@133 69285ed8-183e-4c31-bb72-992939805efb

--- seage.problems/continuous/rosenbrock/src/org/seage/problem/rosenbrock/particles/RosenbrockObjectiveFunction.java
+++ seage.problems/continuous/rosenbrock/src/org/seage/problem/rosenbrock/particles/RosenbrockObjectiveFunction.java
@@ -21,14 +21,31 @@ import org.seage.metaheuristic.particles.Particle;
 public class RosenbrockObjectiveFunction implements IObjectiveFunction
 {
 
-    public void setObjectiveValue(Particle particle)
+    // f(X) = SUM(to N-1, i = 0) [(1 - Xi)^2 + 100 * (Xi+1 - Xi^2)^2]
+    @Deprecated
+    public void setObjectiveValueOld(Particle particle)
     {
-
         double[] coords = particle.getCoords();
+        double value = 0.0;
         for(int i = 0; i < coords.length - 1; i++)
         {
-            // f(X) = SUM(to N-1, i = 0) [(1 - Xi)^2 + 100 * (Xi+1 - Xi^2)^2]
-            particle.setEvaluation( Math.pow(1 - coords[i], 2) + 100 * Math.pow( coords[i + 1] - Math.pow( coords[i], 2 ) , 2 ) );
+            value += (Math.pow(1 - coords[i], 2) + 100 * Math.pow( coords[i + 1] - Math.pow( coords[i], 2 ) , 2 ) );
         }
+        particle.setEvaluation( value );
+        System.out.println("VALUE1:" + value);
+    }
+
+    // f(X) = SUM(to N-1, i = 0) [100*(Xi^2 - Xi+1)^2 + (1 - Xi)^2]
+    // source: http://www.it.lut.fi/ip/evo/functions/node5.html
+    public void setObjectiveValue(Particle particle)
+    {
+        double[] coords = particle.getCoords();
+        double value = 0.0;
+        for(int i = 0; i < coords.length - 1; i++)
+        {
+            value += (100 * Math.pow( Math.pow( coords[i] , 2 ) - coords[i + 1] , 2) + Math.pow( 1 - coords[i] , 2 ));
+        }
+        particle.setEvaluation( value );
+        //System.out.println("VALUE: " + value);
     }
 }
