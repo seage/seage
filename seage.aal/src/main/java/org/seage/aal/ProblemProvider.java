fe4b3408388c1d963f1e590d67092144ba20ca34
Richard Malek <rick@rickq.net>
2010-08-10 00:12:08 +0200

    Changes towards an improved ProblemProvider

---
+++ seage.aal/src/main/java/org/seage/aal/ProblemProvider.java
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Richard Malek and SEAGE contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://seage.sourceforge.net/license/cpl-v10.html
+ *
+ * Contributors:
+ *     Richard Malek
+ *     - Initial implementation
+ */
+package org.seage.aal;
+
+import org.seage.data.DataNode;
+
+/**
+ * Implementation of IProblemProvider interface
+ *
+ * @author Richard Malek
+ */
+public class ProblemProvider implements IProblemProvider
+{
+
+    public Object[][] generateInitialSolutions(int numSolutions) throws Exception
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public DataNode getProblemInfo()
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public IAlgorithmAdapter initAlgorithm(DataNode params)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public IPhenotypeEvaluator initPhenotypeEvaluator() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void initProblemInstance(DataNode params) throws Exception
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void visualizeSolution(Object[] solution) throws Exception
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
