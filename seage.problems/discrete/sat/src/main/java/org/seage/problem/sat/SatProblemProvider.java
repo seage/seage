2daf8abab313ebe25f57cdb759d301268f82c708
Richard Malek <rick@rickq.net>
2010-08-10 02:18:49 +0200

    Basic implementation of ProblemProvider class DONE

---
+++ seage.problems/discrete/sat/src/main/java/org/seage/problem/sat/SatProblemProvider.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.seage.problem.sat;
+
+import org.seage.aal.ProblemProvider;
+
+/**
+ *
+ * @author rick
+ */
+public class SatProblemProvider extends ProblemProvider
+{
+
+}
