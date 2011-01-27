c016e402819dfaa3aa66ff383c913f98aa6f167a
Karel Durkota <durkokar@fel.cvut.cz>
2011-01-27 10:13:25 +0100

    Deleted unnecessary comments

--- seage.problems/discrete/qap/src/main/java/org/seage/problem/qap/sannealing/QapObjectiveFunction.java
+++ seage.problems/discrete/qap/src/main/java/org/seage/problem/qap/sannealing/QapObjectiveFunction.java
@@ -41,32 +41,6 @@ public class QapObjectiveFunction implements IObjectiveFunction
         for(int i=0;i<_matrix[0][0].length;i++){
             addition+=_matrix[2][i][assign[i]];
         }
-
-//        return new double[]{ price+addition };
-
-
-//        double price = 0.0;
-//        int assignLength = _currrentQapSolution.getAssign().length;
-//
-//
-//        double assignPrice = 0;
-//        Double[][][] facilityLocation = _currrentQapSolution.getFacilityLocation();
-//        int numFacilities = facilityLocation[0][0].length;
-//        //System.out.println("My world:["+facilityLocation.length+","+facilityLocation[0].length+","+facilityLocation[0][0].length+"or"+numFacilities+"]");
-//        for(int i=0;i<facilityLocation[0][0].length;i++){
-//            for(int j=0;j<facilityLocation[0][0].length;j++){
-//                double a = facilityLocation[0][i][j];
-//                int b = _currrentQapSolution.getAssign()[i];
-//                int c = _currrentQapSolution.getAssign()[i];
-//                //System.out.println(a+","+b+","+c);
-//                price+=a*facilityLocation[1][b][c];
-//            }
-//        }
-//        double addition=0;
-//        for(int i=0;i<facilityLocation[0][0].length;i++){
-//            addition+=facilityLocation[2][i][_currrentQapSolution.getAssign()[i]];
-//        }
-//        System.out.println("Just calculated fitness is "+(price+addition));
         solution.setObjectiveValue( price+addition );
     }
 }
