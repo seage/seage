062406452c3c444c1edd63aa2e2f35cfa47662cf
Karel Durkota <durkokar@fel.cvut.cz>
2011-01-27 10:13:25 +0100

    Deleted unnecessary comments

--- seage.problems/discrete/qap/src/main/java/org/seage/problem/qap/tabusearch/QapTabuSearchTest.java
+++ seage.problems/discrete/qap/src/main/java/org/seage/problem/qap/tabusearch/QapTabuSearchTest.java
@@ -25,7 +25,7 @@ import org.seage.metaheuristic.tabusearch.TabuSearchListener;
 public class QapTabuSearchTest implements TabuSearchListener
 {
 
-    private static String _dataPath = "F:\\qap\\tai256c.dat";
+    private static String _dataPath = "D:\\tai17a.dat";
 
     public static void main(String[] args)
     {
