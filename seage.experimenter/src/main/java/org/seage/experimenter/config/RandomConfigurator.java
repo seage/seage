48d932843e6773761c92266ba0f8c123387af4e6
Karel Durkota <durkokar@fel.cvut.cz>
2011-02-28 14:35:47 +0100

    Added randomConfigurations deleted problem instances, where optimal solution is unknown (hence impossible to compare with reached solutions with firefly) added different problem instances for qap

---
+++ seage.experimenter/src/main/java/org/seage/experimenter/config/RandomConfigurator.java
@@ -0,0 +1,85 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.seage.experimenter.config;
+
+import java.util.ArrayList;
+import org.seage.aal.data.ProblemConfig;
+import org.seage.aal.data.ProblemInfo;
+import org.seage.data.DataNode;
+
+/**
+ *
+ * @author rick
+ */
+public class RandomConfigurator extends Configurator{
+
+    private String _algID;
+    public static int numberOfExperiments = 20;
+
+    public RandomConfigurator(String algID) {
+        _algID = algID;
+    }
+
+    @Override
+    public ProblemConfig[] prepareConfigs(ProblemInfo problemInfo) throws Exception {
+        ArrayList<ProblemConfig> results = new ArrayList<ProblemConfig>();
+
+//        for(DataNode dn : problemInfo.getDataNode("Algorithms").getDataNodeById(_algID).getDataNodes("Parameter"))
+//        {
+//            
+//        }
+        ProblemConfig[] pattern = new ProblemConfig[numberOfExperiments];
+//        ProblemConfig pattern = new ProblemConfig("Config");
+
+//        DataNode[numberOfExperiments] algorithm = new DataNode[numberOfExperiments];
+        DataNode[] algorithm = new DataNode[numberOfExperiments];
+//        DataNode algorithm = new DataNode("Algorithm");
+//        algorithm.putValue("id", _algID);
+
+        DataNode[] params = new DataNode[numberOfExperiments];
+//        DataNode params = new DataNode("Parameters");
+        for(int i=0;i<params.length;i++){
+            algorithm[i] = new DataNode("Algorithm");
+            algorithm[i].putValue("id",_algID);
+            pattern[i] = new ProblemConfig("Config");
+            params[i]=new DataNode("Parameters");
+            for(DataNode dn : problemInfo.getDataNode("Algorithms").getDataNodeById(_algID).getDataNodes("Parameter")){
+                params[i].putValue(dn.getValueStr("name"), between(dn.getValue("min"),dn.getValue("max")));
+            }
+            algorithm[i].putDataNode(params[i]);
+            pattern[i].putDataNode(algorithm[i]);
+        }
+
+//        algorithm.putDataNode(params);
+//        pattern.putDataNode(algorithm);
+
+
+        for(DataNode inst : problemInfo.getDataNode("Instances").getDataNodes())
+        {
+            for(int i=0;i<numberOfExperiments; i++){
+                ProblemConfig config = (ProblemConfig)pattern[i].clone();
+
+                DataNode problem = new DataNode("Problem");
+                problem.putValue("id", problemInfo.getValue("id"));
+                problem.putDataNode(inst);
+
+                config.putDataNode(problem);
+
+                results.add(config);
+            }
+        }
+
+        return results.toArray(new ProblemConfig[0]);
+    }
+
+    private Object between(Object value1, Object value2) {
+        Double min = (Double)value1;
+        Double max = (Double)value2;
+        Double generated = Math.random()*(max-min)+min;
+        return (Object)generated;
+    }
+
+}
