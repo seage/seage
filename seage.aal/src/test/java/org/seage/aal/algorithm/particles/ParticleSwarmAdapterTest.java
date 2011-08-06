f5529695d4b26aaa6b70fe6560ecdfbc621e39d2
Richard Malek <rick@rickq.net>
2011-08-06 20:49:59 +0200

    Added some testing class to seage.aal

---
+++ seage.aal/src/test/java/org/seage/aal/algorithm/particles/ParticleSwarmAdapterTest.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Richard Malek and SEAGE contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://seage.sourceforge.net/license/cpl-v10.html
+ *
+ * Contributors:
+ *     Richard Malek
+ *     - Initial implementation
+ */
+
+package org.seage.aal.algorithm.particles;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.seage.aal.algorithm.*;
+
+/**
+ *
+ * @author rick
+ */
+public class ParticleSwarmAdapterTest extends AlgorithmAdapterTestBase{
+    
+    @Before
+    public void initAlgorithm()
+    {
+        
+    }
+    
+    @Test
+    public void testAlgorithm() throws Exception
+    {
+        runAlgorithmTest();
+    }
+}
