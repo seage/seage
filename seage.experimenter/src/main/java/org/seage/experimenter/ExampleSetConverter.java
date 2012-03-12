9d462d7079b51c33ed337f90ad3a2990d7cdf76a
Jan Zmatlik <zisanek@gmail.com>
2012-03-12 23:44:29 +0100

    Created conversion from a SEAGE ADT DataNode to a RapidMiner ADT

--- seage.experimenter/src/main/java/org/seage/experimenter/ExampleSetConverter.java
+++ seage.experimenter/src/main/java/org/seage/experimenter/ExampleSetConverter.java
@@ -17,7 +17,6 @@ import com.rapidminer.example.ExampleSet;
 import com.rapidminer.example.table.DataRow;
 import com.rapidminer.example.table.DataRowReader;
 import com.rapidminer.example.table.ExampleTable;
-import com.rapidminer.example.table.NominalMapping;
 import java.util.Iterator;
 import org.seage.data.DataNode;
 
@@ -32,32 +31,41 @@ public class ExampleSetConverter {
      * @param ExampleSet exampleSet is a data structure from RapidMiner API
      * @return DataNode
      */
-    public static DataNode convertToDataNode(ExampleSet exampleSet)
+    public static DataNode convertToDataNode(ExampleSet exampleSet) throws Exception
     {
-        
+        DataNode exampleSetNode = new DataNode("ExampleSet");
+        DataNode attributesNode = new DataNode("Attributes");        
+        DataNode examplesNode = new DataNode("Examples");
+
         // Extracting all of attributes from ExampleSet
-        ////////////////////////////////////////////
         Attributes attributes = exampleSet.getAttributes();
-        Iterator<Attribute> it = attributes.allAttributes();
-        while(it.hasNext())
+        Iterator<Attribute> attributeIterator = attributes.allAttributes();
+        while( attributeIterator.hasNext() )
         {
-            Attribute attb = it.next();
-            System.out.print(attb.getName() + ";");
-        }        
-        System.out.println();
-        ////////////////////////////////////////////        
+            Attribute attribute = attributeIterator.next();
+            
+            DataNode attributeNode = new DataNode("Attribute");
+            attributeNode.putValue("name", attribute.getName());
+            attributesNode.putDataNode( attributeNode );
+        }
                
-        ExampleTable et = exampleSet.getExampleTable();
-        DataRowReader drr = et.getDataRowReader();        
+        ExampleTable exampleTable = exampleSet.getExampleTable();
+        DataRowReader dataRowReader = exampleTable.getDataRowReader();
         
         // Iteration over all examples and over all attributes
-        while(drr.hasNext())
+        while( dataRowReader.hasNext() )
         {
-            DataRow dr = drr.next();
-            it = attributes.allAttributes(); 
-            while(it.hasNext())
+            DataRow dr = dataRowReader.next();
+            
+            DataNode exampleNode = new DataNode("Example");            
+            
+            attributeIterator = attributes.allAttributes(); 
+            
+            while( attributeIterator.hasNext() )
             {
-                Attribute attribute = it.next();
+                Attribute attribute = attributeIterator.next();
+                
+                DataNode resultNode = new DataNode("Result");
                 
                 // According to a javadoc of method get( attribute ), the method returns Double.NaN if a value is not a number (i.e. is null)
                 // But this condition is not true, you can see on the line under this
@@ -67,20 +75,20 @@ public class ExampleSetConverter {
                 if(String.valueOf( dr.get( attribute ) ).equals("NaN") ) continue;
                 
                 if( attribute.isNominal() )
-                {
-                    NominalMapping nm = attribute.getMapping();
-                    System.out.print( nm.mapIndex( (int)dr.get( attribute ) ) + ";");
-                }
-                else                                 
-                    System.out.print( dr.get( attribute ) + ";");
+                    resultNode.putValue("value", attribute.getMapping().mapIndex( (int)dr.get( attribute ) ) );
+                else
+                    resultNode.putValue("value", dr.get( attribute ) );
+                
+                exampleNode.putDataNode( resultNode );
             }
             
-            System.out.println();
-                        
-            //System.out.println(dr.get(attributes.get("ProblemID")));
+            examplesNode.putDataNode( exampleNode );
         }
         
-        return null;
+        exampleSetNode.putDataNode( attributesNode );
+        exampleSetNode.putDataNode( examplesNode );
+        
+        return exampleSetNode;
     }
     
     /**
