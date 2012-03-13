39a59411e4d4b4d70e0634f2debb6b00439eff12
Jan Zmatlik <zisanek@gmail.com>
2012-03-13 14:52:32 +0100

    Refactoring ExampleSetConverter.java, removed generation DataNode to XML file

--- seage.experimenter/src/main/java/org/seage/experimenter/ExampleSetConverter.java
+++ seage.experimenter/src/main/java/org/seage/experimenter/ExampleSetConverter.java
@@ -20,7 +20,6 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import org.seage.data.DataNode;
-import org.seage.data.xml.XmlHelper;
 
 /**
  *
@@ -103,9 +102,7 @@ public class ExampleSetConverter {
         
         exampleSetNode.putDataNode( attributesNode );
         exampleSetNode.putDataNode( examplesNode );
-        
-        XmlHelper.writeXml(exampleSetNode, "C:\\wamp\\"+file);
-        
+
         return exampleSetNode;
     }
     
