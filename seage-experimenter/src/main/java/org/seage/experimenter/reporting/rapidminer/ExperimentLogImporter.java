10faea5f04bb04d247b23d390bd7527a9878a5c3
Richard Malek <rick@rickq.net>
2012-08-12 03:22:51 +0200

    ExperimentLogImporter now uses more generic description of what to extract from algorithm logs

--- seage-experimenter/src/main/java/org/seage/experimenter/reporting/rapidminer/ExperimentLogImporter.java
+++ seage-experimenter/src/main/java/org/seage/experimenter/reporting/rapidminer/ExperimentLogImporter.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -13,6 +14,9 @@ import java.util.zip.ZipFile;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
 import org.seage.thread.TaskRunner;
@@ -49,87 +53,29 @@ public class ExperimentLogImporter
 	private final String RAPIDMINER_LOCAL_REPOSITORY_NAME = "seage";
 	private final String RAPIDMINER_LOCAL_REPOSITORY_DIR_PATH = "repository";
 	
-	MemoryExampleTable _memoryTable;
-	private List<Attribute> _attributes;
-	private List<DataRow> _rmDataRows = new ArrayList<DataRow>();
-
-	public ExperimentLogImporter(String logPath, String repoName) throws RepositoryException
+	private Hashtable<RMDataTableInfo, List<DataRow>> _rmDataTables;
+	
+	public ExperimentLogImporter(String logPath, String repoName) throws RepositoryException, XPathExpressionException
 	{
 		_logPath = logPath;
 		_repoName = repoName;
 		
-		//String rapidMinerHome = "/home/rick/Temp/rm-test";
-		//System.setProperty("rapidminer.home", rapidMinerHome);
+		_rmDataTables = new Hashtable<RMDataTableInfo, List<DataRow>>();
 		
-		_attributes = new ArrayList<Attribute>();
-		_attributes.add(AttributeFactory
-				.createAttribute("ExperimentID", Ontology.NOMINAL));
-		_attributes.add(AttributeFactory
-				.createAttribute("ProblemID", Ontology.NOMINAL));
-		_attributes.add(AttributeFactory
-				.createAttribute("AlgorithmID", Ontology.NOMINAL));
-		_attributes.add(AttributeFactory
-				.createAttribute("InstanceID", Ontology.NOMINAL));
-		_attributes.add(AttributeFactory
-				.createAttribute("ConfigID", Ontology.NOMINAL));
-		_attributes.add(AttributeFactory
-				.createAttribute("RunID", Ontology.NOMINAL));
-		_attributes.add(AttributeFactory
-				.createAttribute("SolutionValue", Ontology.REAL));
+		RMDataTableInfo expValues = new RMDataTableInfo("ExperimentValues");
+		expValues.Attributes.add(new RMAttributeInfo("ExperimentID", Ontology.NOMINAL, "/ExperimentTask/@experimentID"));
+		expValues.Attributes.add(new RMAttributeInfo("ProblemID", Ontology.NOMINAL, "/ExperimentTask/Config/Problem/@id"));
+		expValues.Attributes.add(new RMAttributeInfo("AlgorithmID", Ontology.NOMINAL, "/ExperimentTask/Config/Algorithm/@id"));
+		expValues.Attributes.add(new RMAttributeInfo("InstanceID", Ontology.NOMINAL, "/ExperimentTask/Config/Problem/Instance/@name"));
+		expValues.Attributes.add(new RMAttributeInfo("RunID", Ontology.NOMINAL, "/ExperimentTask/@runID"));
+		expValues.Attributes.add(new RMAttributeInfo("InitSolutionValue", Ontology.REAL, "/ExperimentTask/AlgorithmReport/Statistics/@initObjVal"));
+		expValues.Attributes.add(new RMAttributeInfo("SolutionValue", Ontology.REAL, "/ExperimentTask/AlgorithmReport/Statistics/@bestObjVal"));
+		expValues.Attributes.add(new RMAttributeInfo("NrOfSolutions", Ontology.REAL, "/ExperimentTask/AlgorithmReport/Statistics/@numberOfNewSolutions"));
+		expValues.Attributes.add(new RMAttributeInfo("LastIterNumberNewSol", Ontology.REAL, "/ExperimentTask/AlgorithmReport/Statistics/@lastIterNumberNewSol"));
+		expValues.Attributes.add(new RMAttributeInfo("NrOfIterations", Ontology.REAL, "/ExperimentTask/AlgorithmReport/Statistics/@numberOfIter"));
 		
-	}
-	
-	private class ProcessZipTask implements Runnable
-	{
-		private File _zipFile;
 		
-		private ProcessZipTask(File zipFile)
-		{
-
-			_zipFile = zipFile;
-		}
-
-		@Override
-		public void run()
-		{
-			try
-			{
-				Logger.getLogger(getClass().getName()).log(Level.INFO, Thread.currentThread().getName()+ " - importing file: " + _zipFile.getName());
-				
-				DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-				DocumentBuilder builder = factory.newDocumentBuilder();
-				
-				ZipFile zf = new ZipFile(_zipFile);
-				
-				for (Enumeration<? extends ZipEntry> e = zf.entries(); e.hasMoreElements();)
-				{
-					ZipEntry ze = e.nextElement();
-					String name = ze.getName();
-					if (name.endsWith(".xml"))
-					{
-						InputStream in = zf.getInputStream(ze);
-						// read from 'in'
-						try
-						{
-							Document doc = builder.parse(in);
-							importDocument(doc);
-						}
-						catch (SAXException ex)
-						{
-							Logger.getLogger(getClass().getName()).log(Level.WARNING, ex.getMessage());
-						}
-						// DataNode dn = XmlHelper.readXml(in);
-					}
-				}
-				
-				zf.close();
-			}
-			catch(Exception ex)
-			{
-				_logger.log(Level.SEVERE, ex.getMessage());
-			}
-			
-		}
+		_rmDataTables.put(expValues, new ArrayList<DataRow>());
 		
 	}
 
@@ -161,7 +107,7 @@ public class ExperimentLogImporter
 			ex.printStackTrace();
 		}
 		
-		writeDataTableToRepository();
+		writeDataTablesToRepository();
 
 		long t1 = (System.currentTimeMillis() - t0) / 1000;
 
@@ -169,58 +115,74 @@ public class ExperimentLogImporter
 				"Processing experiment logs DONE - " + t1 + "s");
 	}
 	
-	private synchronized void importDocument(Document doc)
+	private synchronized void importDocument(Document doc) throws XPathExpressionException
 	{
-		Element root = doc.getDocumentElement();
-		Element config = (Element)root.getElementsByTagName("Config").item(0);
-		Element problem = (Element)config.getElementsByTagName("Problem").item(0);
-		Element instance = (Element)problem.getElementsByTagName("Instance").item(0);
-		Element algorithm = (Element)config.getElementsByTagName("Algorithm").item(0);
-		Element algReport = (Element)root.getElementsByTagName("AlgorithmReport").item(0);
-		Element stats = (Element)algReport.getElementsByTagName("Statistics").item(0);
 		
-		double[] arr = new double[7];
-		arr[0] = _attributes.get(0).getMapping().mapString( root.getAttribute("experimentID"));
-		arr[1] = _attributes.get(1).getMapping().mapString( problem.getAttribute("id") );
-		arr[2] = _attributes.get(2).getMapping().mapString( algorithm.getAttribute("id") );
-		arr[3] = _attributes.get(3).getMapping().mapString( instance.getAttribute("name") );
-		arr[4] = _attributes.get(4).getMapping().mapString( config.getAttribute("configID")  );
-		arr[5] = _attributes.get(5).getMapping().mapString( root.getAttribute("runID") );
-		arr[6] = Double.parseDouble(stats.getAttribute("bestObjVal"));
-		
-		_rmDataRows.add(new DoubleArrayDataRow(arr));
+		for(RMDataTableInfo tableInfo : _rmDataTables.keySet())
+		{
+			double[] valArray = new double[tableInfo.Attributes.size()];
+			int i=0;
+			for(RMAttributeInfo attInfo : tableInfo.Attributes)
+			{ 
+				String val = getValueFromDocument(doc.getDocumentElement(), new ArrayList<String>(attInfo.XPath));//(String) xpath.evaluate(attInfo.XPath, doc, XPathConstants.STRING);; //xpath
+				//String val = (String) attInfo.XPath.evaluate( doc, XPathConstants.STRING);; //xpath
+				if(attInfo.Type == Ontology.NOMINAL)
+					valArray[i++] = attInfo.Attribute.getMapping().mapString(val);
+				else
+					valArray[i++] = Double.parseDouble(val);
+			}
+			
+			_rmDataTables.get(tableInfo).add(new DoubleArrayDataRow(valArray));
+		}
 
 	}
 	
-	private synchronized void writeDataTableToRepository() throws OperatorException, OperatorCreationException, RepositoryException
+	private String getValueFromDocument(Element elem, List<String> xPath) {
+		if(xPath.size() == 1)
+			return elem.getAttribute(xPath.get(0));
+		else
+		{
+			String s = xPath.remove(0);
+			return getValueFromDocument((Element)elem.getElementsByTagName(s).item(0), xPath);
+		}
+	}
+
+	private void writeDataTablesToRepository() throws OperatorException, OperatorCreationException, RepositoryException
 	{
 		RapidMiner.setExecutionMode(RapidMiner.ExecutionMode.COMMAND_LINE);
 		RapidMiner.init();
 		
 		initRepository();
 		
-		_memoryTable = new MemoryExampleTable(_attributes);
-		
-		/* Save model */
-		Operator modelWriter = OperatorService
-				.createOperator(RepositoryStorer.class);
-		modelWriter.setParameter(
-				RepositoryStorer.PARAMETER_REPOSITORY_ENTRY,
-				"//"+RAPIDMINER_LOCAL_REPOSITORY_NAME+"/databases/algorithm-logs");
-
-		Process process = new Process();
-
-		process.getRootOperator().getSubprocess(0).addOperator(modelWriter);
-		process.getRootOperator()
-				.getSubprocess(0)
-				.getInnerSources()
-				.getPortByIndex(0)
-				.connectTo(
-						modelWriter.getInputPorts().getPortByName("input"));
-		
-		_memoryTable.readExamples(new ListDataRowReader(_rmDataRows.iterator()));
-
-		process.run(new IOContainer(_memoryTable.createExampleSet()));
+		for(RMDataTableInfo tableInfo : _rmDataTables.keySet())
+		{
+			ArrayList<Attribute> attributes = new ArrayList<Attribute>();
+			for(RMAttributeInfo attInfo : tableInfo.Attributes)
+				attributes.add(attInfo.Attribute);
+			
+			MemoryExampleTable memoryTable = new MemoryExampleTable(attributes);
+			
+			/* Save model */
+			Operator modelWriter = OperatorService
+					.createOperator(RepositoryStorer.class);
+			modelWriter.setParameter(
+					RepositoryStorer.PARAMETER_REPOSITORY_ENTRY,
+					"//"+RAPIDMINER_LOCAL_REPOSITORY_NAME+"/databases/experiments/"+tableInfo.TableName);
+	
+			Process process = new Process();
+	
+			process.getRootOperator().getSubprocess(0).addOperator(modelWriter);
+			process.getRootOperator()
+					.getSubprocess(0)
+					.getInnerSources()
+					.getPortByIndex(0)
+					.connectTo(
+							modelWriter.getInputPorts().getPortByName("input"));
+			
+			memoryTable.readExamples(new ListDataRowReader(_rmDataTables.get(tableInfo).iterator()));
+	
+			process.run(new IOContainer(memoryTable.createExampleSet()));
+		}
 		
 	}
 
@@ -256,6 +218,97 @@ public class ExperimentLogImporter
 		}
 		
 		rm.save();
+		
+	}
+	
+	private class RMDataTableInfo
+	{
+		public String TableName;
+		public List<RMAttributeInfo> Attributes;
+		
+		public RMDataTableInfo(String tableName) {
+			TableName = tableName;
+			Attributes = new ArrayList<RMAttributeInfo>();
+		}
+		
+	}
+	private class RMAttributeInfo
+	{
+		public List<String> XPath;		// Java's XPath is too slow, this is a hack
+		public int Type;
+		public Attribute Attribute;
+		
+		public RMAttributeInfo(String attributeName, int type, String xPath) throws XPathExpressionException 
+		{
+			Type = type;
+			Attribute = AttributeFactory.createAttribute(attributeName, type);			
+			
+			XPath = new ArrayList<String>();
+			String[] split = xPath.split("/"); 
+			for(int i=2;i<split.length;i++) // i=2 -> skip '/ExperimentTask'
+			{
+				if(split[i].startsWith("@"))
+					XPath.add(split[i].substring(1));
+				else
+					XPath.add(split[i]);
+				
+			}
+		}
+		
+	}
+	
+	private class ProcessZipTask implements Runnable
+	{
+		private File _zipFile;
+		
+		private ProcessZipTask(File zipFile)
+		{
+
+			_zipFile = zipFile;
+		}
+
+		@Override
+		public void run()
+		{
+			try
+			{
+				Logger.getLogger(getClass().getName()).log(Level.INFO, Thread.currentThread().getName()+ " - importing file: " + _zipFile.getName());
+				
+				DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+				DocumentBuilder builder = factory.newDocumentBuilder();
+				//XPath xpath = XPathFactory.newInstance().newXPath();
+				
+				ZipFile zf = new ZipFile(_zipFile);
+				
+				for (Enumeration<? extends ZipEntry> e = zf.entries(); e.hasMoreElements();)
+				{
+					ZipEntry ze = e.nextElement();
+					String name = ze.getName();
+					if (name.endsWith(".xml"))
+					{
+						InputStream in = zf.getInputStream(ze);
+						// read from 'in'
+						try
+						{
+							Document doc = builder.parse(in);
+							importDocument(doc);
+						}
+						catch (SAXException ex)
+						{
+							Logger.getLogger(getClass().getName()).log(Level.WARNING, ex.getMessage());
+						}
+						// DataNode dn = XmlHelper.readXml(in);
+					}
+				}
+				
+				zf.close();
+			}
+			catch(Exception ex)
+			{
+				_logger.log(Level.SEVERE, ex.getMessage());
+			}
+			
+		}
 		
 	}
 }
