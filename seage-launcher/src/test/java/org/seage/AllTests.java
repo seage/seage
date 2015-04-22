503fd0ccb75f3352378b8e4a3bff2f353f18e195
Richard Malek <rick@rickq.net>
2015-04-22 15:28:41 +0200

    Added AllTest suite

---
+++ seage-launcher/src/test/java/org/seage/AllTests.java
@@ -0,0 +1,13 @@
+package org.seage;
+
+import org.junit.extensions.cpsuite.ClasspathSuite;
+import org.junit.extensions.cpsuite.ClasspathSuite.IncludeJars;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(ClasspathSuite.class)
+
+public class AllTests
+{
+
+}
