0ceaf05436f7d2ec7fdc11d82ee879acccdf928d
Richard Malek <rick@rickq.net>
2015-04-26 00:47:52 +0200

    Fixed some SAT test classes

--- seage-problems/discrete/sat/src/test/java/org/seage/problem/sat/antcolony/SatGraphTest.java
+++ seage-problems/discrete/sat/src/test/java/org/seage/problem/sat/antcolony/SatGraphTest.java
@@ -1,6 +1,12 @@
 package org.seage.problem.sat.antcolony;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
+import org.seage.aal.problem.IProblemProvider;
+import org.seage.aal.problem.ProblemInstance;
+import org.seage.aal.problem.ProblemInstanceInfo;
+import org.seage.aal.problem.ProblemProvider;
 import org.seage.problem.sat.Formula;
 
 public class SatGraphTest
@@ -8,7 +14,11 @@ public class SatGraphTest
 	@Test
     public void testSatGraph() throws Exception 
     {
-		//Formula f = new Formula();
-		//SatGraph graph = new SatGraph(null);
+		IProblemProvider provider = ProblemProvider.getProblemProviders().get("SAT");
+		ProblemInstanceInfo pii = provider.getProblemInfo().getProblemInstanceInfo("uf20-01");
+		Formula formula = (Formula)provider.initProblemInstance(pii);
+		
+		SatGraph graph = new SatGraph(formula);
+		assertNotNull(graph);
     }
 }
