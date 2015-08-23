438477958b6ea24c9ae5b518885a61bd04024222
Richard Malek <rick@rickq.net>
2015-08-23 23:21:18 +0200

    Removed System.out.println from a unit test

--- seage-problems/discrete/sat/src/test/java/org/seage/problem/sat/FormulaEvaluatorTest.java
+++ seage-problems/discrete/sat/src/test/java/org/seage/problem/sat/FormulaEvaluatorTest.java
@@ -52,10 +52,10 @@ public class FormulaEvaluatorTest
 
     private void printObjectiveValues(FormulaEvaluator formulaEvaluator, Formula f, Boolean[] booleans)
     {
-        double val = 0.0;
-        for (int i = 0; i < booleans.length; i++)
-            val += formulaEvaluator.evaluate(f, (i + 1) * (booleans[i] == true ? 1 : -1));
-        System.out.println(String.format("%.2f - %d", val, FormulaEvaluator.evaluate(f, booleans)));
+//        double val = 0.0;
+//        for (int i = 0; i < booleans.length; i++)
+//            val += formulaEvaluator.evaluate(f, (i + 1) * (booleans[i] == true ? 1 : -1));
+//        System.out.println(String.format("%.2f - %d", val, FormulaEvaluator.evaluate(f, booleans)));
     }
 
 }
