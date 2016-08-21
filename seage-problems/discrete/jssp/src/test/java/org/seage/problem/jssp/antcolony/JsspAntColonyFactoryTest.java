8542c1d7c13af96a12271ba0336f6f09e308b3e2
Richard Malek <rick@rickq.net>
2016-08-21 20:04:45 +0200

    Ignored unit test for unimplemented classes

--- seage-problems/discrete/jssp/src/test/java/org/seage/problem/jssp/antcolony/JsspAntColonyFactoryTest.java
+++ seage-problems/discrete/jssp/src/test/java/org/seage/problem/jssp/antcolony/JsspAntColonyFactoryTest.java
@@ -1,8 +1,10 @@
 package org.seage.problem.jssp.antcolony;
 
+import org.junit.Ignore;
 import org.seage.aal.algorithm.AlgorithmFactoryTestBase;
 import org.seage.problem.jssp.JsspProblemProvider;
 
+@Ignore
 public class JsspAntColonyFactoryTest extends AlgorithmFactoryTestBase
 {
     public JsspAntColonyFactoryTest()
--- seage-problems/discrete/jssp/src/test/java/org/seage/problem/jssp/sannealing/JsspSimulatedAnnealingFactoryTest.java
+++ seage-problems/discrete/jssp/src/test/java/org/seage/problem/jssp/sannealing/JsspSimulatedAnnealingFactoryTest.java
@@ -1,8 +1,10 @@
 package org.seage.problem.jssp.sannealing;
 
+import org.junit.Ignore;
 import org.seage.aal.algorithm.AlgorithmFactoryTestBase;
 import org.seage.problem.jssp.JsspProblemProvider;
 
+@Ignore
 public class JsspSimulatedAnnealingFactoryTest extends AlgorithmFactoryTestBase
 {
     public JsspSimulatedAnnealingFactoryTest()
