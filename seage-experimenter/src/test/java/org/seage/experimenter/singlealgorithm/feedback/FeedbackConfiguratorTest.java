dcfc9025a6934b9eddf7c7471491809e80ad060e
Richard Malek <rick@rickq.net>
2016-08-23 00:02:24 +0200

    Added back (uncommented) a unit test

--- seage-experimenter/src/test/java/org/seage/experimenter/singlealgorithm/feedback/FeedbackConfiguratorTest.java
+++ seage-experimenter/src/test/java/org/seage/experimenter/singlealgorithm/feedback/FeedbackConfiguratorTest.java
@@ -17,26 +17,26 @@ public class FeedbackConfiguratorTest
     //@Test
     public void testPrepareConfigs() throws Exception
     {
-//        FeedbackConfigurator fc = new FeedbackConfigurator();
-//
-//        ProblemInfo pi = new ProblemInfo("TestProblem");
-//        pi.putValue("id", "TestProblem");
-//        pi.putDataNode(new DataNode("Instances"));
-//        pi.putDataNode(new DataNode("Algorithms"));
-//
-//        DataNode ii = new DataNode("Instance");
-//        ii.putValue("id", "Instance01");
-//        ii.putValue("name", "Instance01");
-//        ii.putValue("type", "");
-//        ii.putValue("path", "");
-//        pi.getDataNode("Instances").putDataNode(ii);
-//
-//        DataNode ai = new DataNode("Algorithm");
-//        ai.putValue("id", "Algorithm01");
-//        pi.getDataNode("Algorithms").putDataNode(ai);
-//
-//        ProblemConfig[] pc = fc.prepareConfigs(pi, "Instance01", "Algorithm01", 100);
-//        assertTrue(pc.length == 100);
+        FeedbackConfigurator fc = new FeedbackConfigurator();
+
+        ProblemInfo pi = new ProblemInfo("TestProblem");
+        pi.putValue("id", "TestProblem");
+        pi.putDataNode(new DataNode("Instances"));
+        pi.putDataNode(new DataNode("Algorithms"));
+
+        DataNode ii = new DataNode("Instance");
+        ii.putValue("id", "Instance01");
+        ii.putValue("name", "Instance01");
+        ii.putValue("type", "");
+        ii.putValue("path", "");
+        pi.getDataNode("Instances").putDataNode(ii);
+
+        DataNode ai = new DataNode("Algorithm");
+        ai.putValue("id", "Algorithm01");
+        pi.getDataNode("Algorithms").putDataNode(ai);
+
+        ProblemConfig[] pc = fc.prepareConfigs(pi, "Instance01", "Algorithm01", 100);
+        assertTrue(pc.length == 100);
 
     }
 
