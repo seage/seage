840837ba657df3e27b21d47835f9debf25fc745f
Richard Malek <rick@rickq.net>
2021-01-15 22:37:42 +0100

    Disabled DbManagerTest

--- seage-hyperheuristics/src/test/java/org/seage/hh/knowledgebase/importing/db/dao/DbManagerTest.java
+++ seage-hyperheuristics/src/test/java/org/seage/hh/knowledgebase/importing/db/dao/DbManagerTest.java
@@ -1,19 +1,21 @@
 package org.seage.hh.knowledgebase.importing.db.dao;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.apache.ibatis.session.SqlSession;
+
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
 import org.seage.hh.knowledgebase.db.dao.DbManager;
 import org.seage.hh.knowledgebase.db.dao.DbManager.DbType;
 
+@Disabled("Disabled for now")
 class DbManagerTest {
 
-    @Test
-    void test() throws Exception {
-        DbManager.init(DbType.POSTGRES);
-        SqlSession session = DbManager.getSqlSessionFactory().openSession();
-        assertNotNull(session);
-    }
+  @Test
+  void test() throws Exception {
+    DbManager.init(DbType.POSTGRES);
+    SqlSession session = DbManager.getSqlSessionFactory().openSession();
+    // assertNotNull(session);
+  }
 
 }
