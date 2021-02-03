3d333d3747e0c5485a8266e46d4b8ee7b09a1e66
Richard Malek <rick@rickq.net>
2021-02-03 18:09:07 +0100

    #21 Bug reproduced, test fails

--- seage-problems/discrete/tsp/src/test/java/org/seage/problem/tsp/sannealing/TspSimulatedAnnealingAdapterTest.java
+++ seage-problems/discrete/tsp/src/test/java/org/seage/problem/tsp/sannealing/TspSimulatedAnnealingAdapterTest.java
@@ -5,7 +5,7 @@ import org.seage.aal.problem.ProblemAlgorithmAdapterTestBase;
 import org.seage.problem.tsp.TspPhenotype;
 import org.seage.problem.tsp.TspProblemProvider;
 
-@Disabled("Adapter class not fully implemented yet")
+// @Disabled("Adapter class not fully implemented yet")
 public class TspSimulatedAnnealingAdapterTest extends ProblemAlgorithmAdapterTestBase<TspPhenotype> {
   public TspSimulatedAnnealingAdapterTest() {
     super(new TspProblemProvider(), "SimulatedAnnealing");
