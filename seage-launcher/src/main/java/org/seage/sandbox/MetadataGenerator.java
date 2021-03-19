f2e0432d379bd6dc48b542bfc052339dc1658884
David Omrai <david.omrai@seznam.cz>
2021-03-20 00:04:00 +0100

    #33 Fixed all eslint warnings and prepared some space for method description comments

--- seage-launcher/src/main/java/org/seage/sandbox/MetadataGenerator.java
+++ seage-launcher/src/main/java/org/seage/sandbox/MetadataGenerator.java
@@ -25,147 +25,182 @@ package org.seage.sandbox;
 import java.io.InputStream;
 import java.util.Arrays;
 import java.util.Random;
-import org.apache.hadoop.hdfs.protocol.DatanodeAdminProperties;
 import org.seage.aal.problem.ProblemInstance;
 import org.seage.aal.problem.ProblemInstanceInfo;
 import org.seage.aal.problem.ProblemInstanceInfo.ProblemInstanceOrigin;
 
+import org.seage.data.DataNode;
+
+import org.seage.problem.sat.Formula;
+import org.seage.problem.sat.FormulaReader;
+import org.seage.problem.sat.SatPhenotypeEvaluator;
+import org.seage.problem.sat.SatProblemProvider;
 
 import org.seage.problem.tsp.City;
 import org.seage.problem.tsp.CityProvider;
 import org.seage.problem.tsp.TourProvider;
-import org.seage.problem.tsp.TspPhenotypeEvaluator;
 import org.seage.problem.tsp.TspPhenotype;
+import org.seage.problem.tsp.TspPhenotypeEvaluator;
 
-import org.seage.problem.sat.Formula;
-import org.seage.problem.sat.SatPhenotypeEvaluator;
-import org.seage.problem.sat.SatProblemProvider;
-import org.seage.problem.sat.FormulaReader;
 
-import org.seage.data.DataNode;
 
 /**
- * 
+ * .
  * @author David Omrai
  */
 
 public class MetadataGenerator {
-    public static void main(String[] args) {
-        try {
-          String[] tspInstancesID = new String[]{
-            "hyflex-tsp-0",
-            "hyflex-tsp-8",
-            "hyflex-tsp-2",
-            "hyflex-tsp-7",
-            "hyflex-tsp-6"
-          };
-          String[] satInstancesID = new String[]{
-            "hyflex-sat-3",
-            "hyflex-sat-5",
-            "hyflex-sat-4",
-            "hyflex-sat-10",
-            "hyflex-sat-11"
-          };
-    
-          new MetadataGenerator().run(tspInstancesID, satInstancesID);
-        } catch (Exception ex) {
-          ex.printStackTrace();
-        }
+  
+  /**
+   * .
+   * @param args .
+   */
+  public static void main(String[] args) {
+    try {
+      String[] tspInstancesID = new String[]{
+        "hyflex-tsp-0",
+        "hyflex-tsp-8",
+        "hyflex-tsp-2",
+        "hyflex-tsp-7",
+        "hyflex-tsp-6"
+      };
+      String[] satInstancesID = new String[]{
+        "hyflex-sat-3",
+        "hyflex-sat-5",
+        "hyflex-sat-4",
+        "hyflex-sat-10",
+        "hyflex-sat-11"
+      };
+
+      new MetadataGenerator().run(tspInstancesID, satInstancesID);
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  /**
+   * .
+   * @param tspInstancesID . 
+   * @param satInstancesID .
+   */
+  public void run(String[] tspInstancesID, String[] satInstancesID) throws Exception {
+    double[] tspMedianResults = tspMetaGenerator(10000, tspInstancesID);
+    double[] satMedianResults = satMetaGenerator(10000, satInstancesID);
+
+    DataNode results = new DataNode("results");
+
+    DataNode tspRes = new DataNode("tsp-problem");
+    for (int i = 0; i < tspInstancesID.length; i++) {
+      DataNode inst = new DataNode("instance");
+      inst.putValue(tspInstancesID[i], Double.toString(tspMedianResults[i]));
+
+      tspRes.putDataNode(inst);
+    }
+    results.putDataNode(tspRes);
+
+    DataNode satRes = new DataNode("sat-problem");
+    for (int i = 0; i < satInstancesID.length; i++) {
+      DataNode inst = new DataNode("instance");
+      inst.putValue(satInstancesID[i], Double.toString(satMedianResults[i]));
+
+      satRes.putDataNode(inst);
+    }
+    results.putDataNode(satRes);
+
+
+    DataNode mdGenRes = new DataNode("MetadataGenerator");
+    mdGenRes.putDataNode(results);
+    System.out.println(mdGenRes.getDataNodes());
+    System.out.println(mdGenRes.toString());
+  }
+
+
+  /**
+   * .
+   * @param array .
+   * @return
+   */
+  public static double median(double[] array) {
+    if (array.length == 1) {
+      return array[0];
+    }
+
+    Arrays.sort(array);
+    if (array.length % 2 == 0) {
+      return (((double)array[array.length / 2] + (double)array[array.length / 2 - 1]) / 2);
+    }
+    return ((double)array[ (array.length / 2) + 1 ]);
+  }
+
+  /**
+   * .
+   * @param populationCount .
+   * @param instancesID .
+   * @return
+   */
+  public double[] tspMetaGenerator(int populationCount, String[] instancesID) throws Exception {
+    double[] results = new double[instancesID.length];
+  
+    //iterate through all instances
+    for (int ins = 0; ins < instancesID.length; ins++) {
+      String path = String.format("/org/seage/problem/tsp/instances/%s.tsp", instancesID[ins]);
+      City[] cities = null;
+
+      try (InputStream stream = getClass().getResourceAsStream(path)) {    
+        cities = CityProvider.readCities(stream);
       }
-    
-      public void run(String[] tspInstancesID, String[] satInstancesID) throws Exception {
-        double[] tspMedianResults = tspMetaGenerator(10000, tspInstancesID);
-        double[] satMedianResults = satMetaGenerator(10000, satInstancesID);
 
-        DataNode mdGenRes = new DataNode("MetadataGenerator");
-        DataNode results = new DataNode("results");
-
-        DataNode tspRes = new DataNode("tsp-problem");
-        for (int i = 0; i < tspInstancesID.length; i++){
-          DataNode inst = new DataNode("instance");
-          inst.putValue(tspInstancesID[i], Double.toString(tspMedianResults[i]));
-
-          tspRes.putDataNode(inst);
-        }
-
-        results.putDataNode(tspRes);
-
-        DataNode satRes = new DataNode("sat-problem");
-        for (int i = 0; i < satInstancesID.length; i++) {
-          DataNode inst = new DataNode("instance");
-          inst.putValue(satInstancesID[i], Double.toString(satMedianResults[i]));
-
-          satRes.putDataNode(inst);
-        }
-
-        results.putDataNode(satRes);
-        mdGenRes.putDataNode(results);
-        System.out.println(mdGenRes.getDataNodes());
-        System.out.println(mdGenRes.toXml());
-      }
-
-
-      public static double median(double[] array) {
-        Arrays.sort(array);
-        if (array.length % 2 == 0)
-          return ((double)array[array.length/2] + (double)array[array.length/2 -1]/2);
-        return((double)array[array.length/2]);
-      }
-
-      public double[] tspMetaGenerator( int populationCount, String[] instancesID ) throws Exception {
-        double[] results = new double[instancesID.length];
+      // System.out.println("cities len " + cities.length);
+      // System.out.println("Population: " + populationCount);
       
-        //iterate through all instances
-        for (int ins = 0; ins < instancesID.length; ins++){
-          String path = String.format("/org/seage/problem/tsp/instances/%s.tsp", instancesID[ins]);
-          City[] cities = null;
+      double[] randomResults = new double[populationCount];
 
-          try(InputStream stream = getClass().getResourceAsStream(path)) {    
-            cities = CityProvider.readCities(stream);
-          }
+      TspPhenotypeEvaluator tspEval = new TspPhenotypeEvaluator(cities);
 
-          // System.out.println("cities len " + cities.length);
-          // System.out.println("Population: " + populationCount);
-          
-          double[] randomResults = new double[populationCount];
-
-          TspPhenotypeEvaluator tspEval = new TspPhenotypeEvaluator(cities);
-
-          for (int i = 0; i < populationCount; i++) {
-            randomResults[i] = tspEval.evaluate(new TspPhenotype(TourProvider.createRandomTour(cities.length)))[0];
-          }
-
-          results[ins] = median(randomResults);
-        } 
-        return results;
+      for (int i = 0; i < populationCount; i++) {
+        randomResults[i] = tspEval
+        .evaluate(new TspPhenotype(TourProvider.createRandomTour(cities.length)))[0];
       }
 
-      public double[] satMetaGenerator( int populationCount, String[] instancesID ) throws Exception {
-        double[] results = new double[instancesID.length];
+      results[ins] = median(randomResults);
+    } 
+    return results;
+  }
+
+  /**
+   * .
+   * @param populationCount .
+   * @param instancesID .
+   * @return
+   */
+  public double[] satMetaGenerator(int populationCount, String[] instancesID)
+       throws Exception {
+    double[] results = new double[instancesID.length];
+  
+    //iterate through all instances
+    
+    for (int ins = 0; ins < instancesID.length; ins++) {
+      String path = String.format("/org/seage/problem/sat/instances/%s.cnf", instancesID[ins]);
+
+      Formula formula = null;
       
-        //iterate through all instances
-       
-        for (int ins = 0; ins < instancesID.length; ins++){
-          String path = String.format("/org/seage/problem/sat/instances/%s.cnf", instancesID[ins]);
-
-          Formula formula = null;
-          
-          try(InputStream stream = getClass().getResourceAsStream(path)) {    
-            formula = new Formula(new ProblemInstanceInfo("", ProblemInstanceOrigin.FILE, path),
-            FormulaReader.readClauses(stream));
-          }
-
-          double[] randomResults = new double[populationCount];
-
-          SatPhenotypeEvaluator satEval = new SatPhenotypeEvaluator(formula);
-
-          for (int i = 0; i < populationCount; i++) {
-            randomResults[i] = satEval.evaluate(
-              new SatProblemProvider().generateInitialSolutions((ProblemInstance)formula, 1, new Random().nextLong())[0] )[0];
-          }
-
-          results[ins] = median(randomResults);
-        } 
-        return results;
+      try (InputStream stream = getClass().getResourceAsStream(path)) {
+        formula = new Formula(new ProblemInstanceInfo("", ProblemInstanceOrigin.FILE, path),
+        FormulaReader.readClauses(stream));
       }
+
+      double[] randomResults = new double[populationCount];
+
+      SatPhenotypeEvaluator satEval = new SatPhenotypeEvaluator(formula);
+
+      for (int i = 0; i < populationCount; i++) {
+        randomResults[i] = satEval.evaluate(
+          new SatProblemProvider()
+          .generateInitialSolutions((ProblemInstance)formula, 1, new Random().nextLong())[0])[0];
+      }
+
+      results[ins] = median(randomResults);
+    } 
+    return results;
+  }
 }
