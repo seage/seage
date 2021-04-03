fea43be971b77f69587fcaf810d1807bcd621ed2
David Omrai <david.omrai@seznam.cz>
2021-04-03 20:22:34 +0200

    #39 Fixed exception

--- seage-launcher/src/main/java/org/seage/launcher/commands/ProblemInstanceParamsParser.java
+++ seage-launcher/src/main/java/org/seage/launcher/commands/ProblemInstanceParamsParser.java
@@ -64,7 +64,7 @@ public class ProblemInstanceParamsParser {
           break;
         
         default:
-          new Exception("Bad problemID");
+          throw new Exception("Bad problemID");
       }
       return;
     }   
