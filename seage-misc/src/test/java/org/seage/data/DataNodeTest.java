d55e123c753b88aee939965401c00e2011354d05
Richard Malek <rick@rickq.net>
2021-04-03 14:07:30 +0200

    #37 Extended tests for DataNode

--- seage-misc/src/test/java/org/seage/data/DataNodeTest.java
+++ seage-misc/src/test/java/org/seage/data/DataNodeTest.java
@@ -56,4 +56,28 @@ public class DataNodeTest {
     assertEquals(2, dn11.getValueInt("a1"));
     assertEquals(3, dn22.getValueInt("a1"));
   }
+
+  @Test
+  void testDataNodeCopyConstructorMoreStructure() throws Exception {
+    DataNode dn1 = new DataNode("dn1");
+    DataNode dn11 = new DataNode("dn11");
+    DataNode dn111 = new DataNode("dn111");
+    
+    dn111.putValue("a1", 1);
+    dn11.putDataNode(dn111);
+    dn1.putDataNode(dn11);    
+
+    DataNode dn2 = new DataNode(dn1);
+    DataNode dn222 = dn2.getDataNode("dn11").getDataNode("dn111");
+
+    assertEquals(1, dn222.getValueInt("a1"));
+
+    dn111.putValue("a1", 2);
+    assertEquals(2, dn111.getValueInt("a1"));
+    assertEquals(1, dn222.getValueInt("a1"));
+
+    dn222.putValue("a1", 3);
+    assertEquals(2, dn111.getValueInt("a1"));
+    assertEquals(3, dn222.getValueInt("a1"));
+  }
 }
