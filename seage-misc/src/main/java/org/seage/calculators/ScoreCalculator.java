3a7b4458c3a9b109aef44c82c51bcd8c8c899824
David Omrai <david.omrai@seznam.cz>
2021-04-03 11:25:12 +0200

    #38 Prepared code for unit metric score calculator

--- seage-misc/src/main/java/org/seage/calculators/ScoreCalculator.java
+++ seage-misc/src/main/java/org/seage/calculators/ScoreCalculator.java
@@ -1,5 +1,8 @@
 package org.seage.calculators;
 
+import java.util.List;
+import org.seage.data.DataNode;
+
 public interface ScoreCalculator {
-    
+  public List<DataNode> calculateScore(List<DataNode> card) throws Exception;
 }
--- seage-misc/src/main/java/org/seage/calculators/UnitMetricScoreCalculator.java
+++ seage-misc/src/main/java/org/seage/calculators/UnitMetricScoreCalculator.java
@@ -1,5 +1,25 @@
 package org.seage.calculators;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.seage.data.DataNode;
+
 public class UnitMetricScoreCalculator implements ScoreCalculator {
-    
+  /**
+   * Map represents weights for each problem domain.
+   */
+  @SuppressWarnings("serial")
+  private final Map<String, Double> problemsWeightsMap = new HashMap<>() {
+    {
+      put("SAT", 1.0);
+      put("TSP", 1.0);
+    }
+  };
+
+  @Override
+  public List<DataNode> calculateScore(List<DataNode> card) throws Exception {
+    //todo
+    return null;
+  }
 }
