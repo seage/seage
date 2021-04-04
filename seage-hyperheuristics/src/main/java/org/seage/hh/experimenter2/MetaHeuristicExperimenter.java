65c8896cff4ac61d9c87df2d830de63ef9b68e20
Richard Malek <rick@rickq.net>
2021-04-05 00:47:52 +0200

    #46 Cleaned MetaHeuristicExperimenter

--- seage-hyperheuristics/src/main/java/org/seage/hh/experimenter2/MetaHeuristicExperimenter.java
+++ seage-hyperheuristics/src/main/java/org/seage/hh/experimenter2/MetaHeuristicExperimenter.java
@@ -1,11 +1,8 @@
 package org.seage.hh.experimenter2;
 
-import java.time.Instant;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.UUID;
-import java.util.concurrent.TimeUnit;
 import org.seage.aal.problem.ProblemConfig;
 import org.seage.aal.problem.ProblemInfo;
 import org.seage.aal.problem.ProblemInstanceInfo;
@@ -55,55 +52,19 @@ public class MetaHeuristicExperimenter implements AlgorithmExperimenter {
     this.experimentReporter = experimentReporter;
 
     // Initialize all
-    logger.info("Experimenter {} created, getting problem info", experimentID);
     this.problemInfo = ProblemProvider.getProblemProviders().get(this.problemID).getProblemInfo();
     this.experimentTasksRunner = new LocalExperimentTasksRunner();
     this.configurator = new DefaultConfigurator(0.15);
   }
 
-
   /**
    * Method runs experiment.
    */
   public void runExperiment() {
-    logger.info("Running MetaheuristicExperimenter");
     try {
-      logger.info("-------------------------------------");
-      logger.info("Experimenter: {}", this.experimentName);
-      logger.info("ExperimentID: {}", experimentID);
-      logger.info("-------------------------------------");
-
-      long totalNumOfConfigs = getNumberOfConfigs(1, 1);
-      long totalRunsPerCpu = (long)Math.ceil(
-          (double)totalNumOfConfigs / Runtime.getRuntime().availableProcessors());
-      long totalEstimatedTime = getEstimatedTime(1, 1);
-
-      logger.info(String.format("%-25s: %s", "Total number of configs", totalNumOfConfigs));
-      logger.info("Total number of configs per cpu core: " + totalRunsPerCpu);
-      logger.info(String.format("Total estimated time: %s (DD:HH:mm:ss)", 
-          getDurationBreakdown(totalEstimatedTime)));
-      logger.info("-------------------------------------");
-
-      
-      // Run experiment
-      logger.info("-------------------------------------");
-      logger.info(String.format("%-15s %s", "Problem:", problemID));
-      logger.info(String.format("Instance: " + instanceID));
-
       ProblemInstanceInfo instanceInfo = problemInfo.getProblemInstanceInfo(instanceID);
       runExperimentTasksForProblemInstance(instanceInfo);
-      // end experiment
 
-      // Inform about the duration
-      long startDate = System.currentTimeMillis();
-      long endDate = startDate;
-      endDate = System.currentTimeMillis();
-      logger.info("-------------------------------------");
-      logger.info("Experiment {} finished ...", experimentID);
-      logger.info("Experiment duration: {} (DD:HH:mm:ss)", 
-          getDurationBreakdown(endDate - startDate));
-      
-      experimentReporter.updateEndDate(this.experimentID, new Date(endDate));
     } catch (Exception ex) {
       logger.warn(ex.getMessage(), ex);
     }
@@ -112,8 +73,6 @@ public class MetaHeuristicExperimenter implements AlgorithmExperimenter {
 
   protected void runExperimentTasksForProblemInstance(
       ProblemInstanceInfo instanceInfo) throws Exception {
-    // Inform about the beginning
-    logger.info(String.format("%-44s", "   Running... "));
 
     // Initialize the best value 
     double bestObjVal = Double.MAX_VALUE;
@@ -149,7 +108,6 @@ public class MetaHeuristicExperimenter implements AlgorithmExperimenter {
     experimentReporter.updateScore(experimentID, bestObjVal);
   }
 
-
   private Void reportExperimentTask(ExperimentTask experimentTask) {
     try {
       experimentReporter.reportExperimentTask(experimentTask);
@@ -158,43 +116,5 @@ public class MetaHeuristicExperimenter implements AlgorithmExperimenter {
           experimentTask.getExperimentTaskID().toString()), e);
     }
     return null;
-  }
-
-
-  protected String getExperimentConfig() {
-    DataNode config = new DataNode("Config");
-    config.putValue("timeoutS", this.timeoutS);
-    config.putValue("numRuns", this.numRuns);
-    
-    return config.toString();
-  }
-
-
-  protected static String getDurationBreakdown(long millis) {
-    if (millis < 0) {
-      throw new IllegalArgumentException("Duration must be greater than zero!");
-    }
-
-    long days = TimeUnit.MILLISECONDS.toDays(millis);
-    millis -= TimeUnit.DAYS.toMillis(days);
-    long hours = TimeUnit.MILLISECONDS.toHours(millis);
-    millis -= TimeUnit.HOURS.toMillis(hours);
-    long minutes = TimeUnit.MILLISECONDS.toMinutes(millis);
-    millis -= TimeUnit.MINUTES.toMillis(minutes);
-    long seconds = TimeUnit.MILLISECONDS.toSeconds(millis);
-
-    return String.format("%02d:%02d:%02d:%02d", days, hours, minutes, seconds); // (sb.toString());
-  }
-  
-
-  protected long getNumberOfConfigs(int instancesCount, int algorithmsCount) {
-    return (long)this.numRuns * (long)this.numRuns * instancesCount * algorithmsCount;
-  }
-
-
-  protected long getEstimatedTime(int instancesCount, int algorithmsCount) {
-    return (long)Math.ceil((double)getNumberOfConfigs(instancesCount, algorithmsCount) 
-        / Runtime.getRuntime().availableProcessors())
-        * this.timeoutS * 1000;
   }
 }
