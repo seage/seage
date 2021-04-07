e6dc418399827eee2809bcdae41066299b9cafcb
David Omrai <david.omrai@seznam.cz>
2021-04-07 23:11:05 +0200

    #48 Prettified the code and made it a little bit more clear for understanding

--- seage-aal/src/test/java/org/seage/aal/problem/ProblemScoreCalculatorTest.java
+++ seage-aal/src/test/java/org/seage/aal/problem/ProblemScoreCalculatorTest.java
@@ -12,6 +12,7 @@ public class ProblemScoreCalculatorTest {
   protected ProblemScoreCalculator problemScoreCalculator;
   protected static ProblemInfo singleProblemInfo;
   
+
   @BeforeAll
   static void init() throws Exception {
     ProblemProvider.providerClasses =
@@ -29,6 +30,7 @@ public class ProblemScoreCalculatorTest {
     singleProblemInfo = new ProblemInfo("TEST");
     singleProblemInfo.putDataNode(ins);
   }
+
 
   @Test
   public void testCalculatingOptimalInstanceScore() throws Exception {
@@ -54,6 +56,7 @@ public class ProblemScoreCalculatorTest {
         0.0, problemScoreCalculator.calculateInstanceScore("test-instance", random), 0.1);
   }
 
+
   @Test
   public void testCalculatingMidsectionInstanceScore() throws Exception {
     problemScoreCalculator = new ProblemScoreCalculator(singleProblemInfo);
@@ -68,6 +71,7 @@ public class ProblemScoreCalculatorTest {
         0.5, problemScoreCalculator.calculateInstanceScore("test-instance", midsection), 0.1);
   }
 
+
   @Test
   public void testCalculatingBetterThanOptimumInstanceScore() throws Exception {
     problemScoreCalculator = new ProblemScoreCalculator(singleProblemInfo);
@@ -80,6 +84,7 @@ public class ProblemScoreCalculatorTest {
         () -> problemScoreCalculator.calculateInstanceScore("test-instance", optimum - 1));
   }
 
+
   @Test
   public void testCalculatingWorseThanRandomInstanceScore() throws Exception {
     problemScoreCalculator = new ProblemScoreCalculator(singleProblemInfo);
@@ -91,79 +96,80 @@ public class ProblemScoreCalculatorTest {
         problemScoreCalculator.calculateInstanceScore("test-instance", random + 1), 0.1);
   }
 
+
   @Test
   public void testCalculatingProblemScore() throws Exception {
-    String[] names = {"1", "2", "3", "4", "5"};
-    double[] values = {0.4, 0.2, 0.1, 0.6, 0.3};
-    double[] sizes = {10.0, 11.0, 12.0, 13.0, 15.0};
+    String[] instanceIDs = {"1", "2", "3", "4", "5"};
+    double[] objValues = {0.4, 0.2, 0.1, 0.6, 0.3};
+    double[] instanceSizes = {10.0, 11.0, 12.0, 13.0, 15.0};
 
     DataNode ins = new DataNode("Instances");
 
-    for (int i = 0; i < names.length; i++) {
-      DataNode dn = new DataNode(names[i]);
-      dn.putValue("id", names[i]);
+    for (int i = 0; i < instanceIDs.length; i++) {
+      DataNode dn = new DataNode(instanceIDs[i]);
+      dn.putValue("id", instanceIDs[i]);
       dn.putValue("type", "resource");
       dn.putValue("path", "");
       dn.putValue("optimum", 2.0);
       dn.putValue("random", 42.0);
-      dn.putValue("size", sizes[i]);
+      dn.putValue("size", instanceSizes[i]);
 
       ins.putDataNode(dn);
     }
     
     ProblemInfo problemInfo = new ProblemInfo("TEST");
     problemInfo.putDataNode(ins);
-
-
     problemScoreCalculator = new ProblemScoreCalculator(problemInfo);
 
 
-    int arrayLength = names.length;
-
+    int arrayLength = instanceIDs.length;
     double numerator = 0.0;
     double denominator = 0.0;
+
     for (int i = 0; i < arrayLength; i++) {
       // Weight
       double instanceSize = problemInfo
-          .getProblemInstanceInfo(names[i]).getValueDouble("size");
+          .getProblemInstanceInfo(instanceIDs[i]).getValueDouble("size");
       
-      numerator += instanceSize * values[i];
+      numerator += instanceSize * objValues[i];
       denominator += instanceSize;
     }
 
     double weightedMean = numerator / denominator;
 
-    assertEquals(weightedMean, problemScoreCalculator.calculateProblemScore(names, values), 0.1);
+    assertEquals(
+        weightedMean, problemScoreCalculator.calculateProblemScore(instanceIDs, objValues), 0.1);
   }
+
 
   @Test
   public void testCalculatingBasicMeanOfScores() throws Exception {
-    String[] names = {"1", "2", "3", "4", "5"};
-    double[] sizes = {1.0, 1.0, 1.0, 1.0, 1.0};
+    String[] instanceIDs = {"1", "2", "3", "4", "5"};
+    double[] instanceSizes = {1.0, 1.0, 1.0, 1.0, 1.0};
 
     DataNode ins = new DataNode("Instances");
 
-    for (int i = 0; i < names.length; i++) {
-      DataNode dn = new DataNode(names[i]);
-      dn.putValue("id", names[i]);
+    for (int i = 0; i < instanceIDs.length; i++) {
+      DataNode dn = new DataNode(instanceIDs[i]);
+      dn.putValue("id", instanceIDs[i]);
       dn.putValue("type", "resource");
       dn.putValue("path", "");
       dn.putValue("optimum", 2.0);
       dn.putValue("random", 42.0);
-      dn.putValue("size", sizes[i]);
+      dn.putValue("size", instanceSizes[i]);
 
       ins.putDataNode(dn);
     }
     
     ProblemInfo problemInfo = new ProblemInfo("TEST");
     problemInfo.putDataNode(ins);
-
-
     problemScoreCalculator = new ProblemScoreCalculator(problemInfo);
 
-    double[] values = {0.4, 0.2, 0.1, 0.6, 0.3};
-    double mean = Arrays.stream(values).sum() / values.length;
+    
+    double[] objValues = {0.4, 0.2, 0.1, 0.6, 0.3};
+    double mean = Arrays.stream(objValues).sum() / objValues.length;
 
-    assertEquals(mean, problemScoreCalculator.calculateProblemScore(names, values), 0.1);
+    assertEquals(
+        mean, problemScoreCalculator.calculateProblemScore(instanceIDs, objValues), 0.1);
   }
 }
