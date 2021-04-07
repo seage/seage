f58627561f90f4f812ed5041da1d3441ee80b104
David Omrai <david.omrai@seznam.cz>
2021-04-07 16:48:28 +0200

    #48 Fixed and finished the tests

--- seage-aal/src/test/java/org/seage/aal/problem/ProblemScoreCalculatorTest.java
+++ seage-aal/src/test/java/org/seage/aal/problem/ProblemScoreCalculatorTest.java
@@ -1,14 +1,12 @@
 package org.seage.aal.problem;
 
 import java.util.Arrays;
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import org.seage.aal.algorithm.Phenotype;
 import org.seage.data.DataNode;
 
 public class ProblemScoreCalculatorTest {
@@ -22,14 +20,6 @@ public class ProblemScoreCalculatorTest {
 
   @Test
   public void testCalculatingOptimalInstanceScore() throws Exception {
-    // Map<String, IProblemProvider<Phenotype<?>>> providers = ProblemProvider.getProblemProviders();
-
-    // System.out.println(providers.keySet());
-    // IProblemProvider<?> pp = providers.get("TEST");
-
-    // ProblemInfo problemInfo = pp.getProblemInfo();
-
-    ProblemInfo problemInfo = new ProblemInfo("TEST");
     DataNode dn = new DataNode("test-instance");
     dn.putValue("id", "test-instance");
     dn.putValue("type", "resource");
@@ -38,87 +28,152 @@ public class ProblemScoreCalculatorTest {
     dn.putValue("random", 42.0);
     DataNode ins = new DataNode("Instances");
     ins.putDataNode(dn);
+    
+    ProblemInfo problemInfo = new ProblemInfo("TEST");
     problemInfo.putDataNode(ins);
 
     problemScoreCalculator = new ProblemScoreCalculator(problemInfo);
 
     double optimum = problemInfo.getProblemInstanceInfo("test-instance").getValueDouble("optimum");
-
-    System.out.println(optimum);
    
-    assertEquals(1.0, problemScoreCalculator.calculateInstanceScore("test-instance", optimum), 0.001);
+    assertEquals(
+        1.0, problemScoreCalculator.calculateInstanceScore("test-instance", optimum), 0.1);
   }
 
 
   @Test
   public void testCalculatingRandomInstanceScore() throws Exception {
-    Map<String, IProblemProvider<Phenotype<?>>> providers = ProblemProvider.getProblemProviders();
-    IProblemProvider<?> pp = providers.get("SAT");
-    ProblemInfo problemInfo = pp.getProblemInfo();
+    DataNode dn = new DataNode("test-instance");
+    dn.putValue("id", "test-instance");
+    dn.putValue("type", "resource");
+    dn.putValue("path", "");
+    dn.putValue("optimum", 2.0);
+    dn.putValue("random", 42.0);
+    DataNode ins = new DataNode("Instances");
+    ins.putDataNode(dn);
+    
+    ProblemInfo problemInfo = new ProblemInfo("TEST");
+    problemInfo.putDataNode(ins);
 
     problemScoreCalculator = new ProblemScoreCalculator(problemInfo);
 
-    double random = problemInfo.getProblemInstanceInfo("uf100-0169").getValueDouble("random");
+    double random = problemInfo.getProblemInstanceInfo("test-instance").getValueDouble("random");
     
-    assertEquals(0.0, problemScoreCalculator.calculateInstanceScore("uf100-169", random), 0.001);
+    assertEquals(
+        0.0, problemScoreCalculator.calculateInstanceScore("test-instance", random), 0.1);
   }
 
   @Test
   public void testCalculatingMidsectionInstanceScore() throws Exception {
-    Map<String, IProblemProvider<Phenotype<?>>> providers = ProblemProvider.getProblemProviders();
-    IProblemProvider<?> pp = providers.get("SAT");
-    ProblemInfo problemInfo = pp.getProblemInfo();
+    DataNode dn = new DataNode("test-instance");
+    dn.putValue("id", "test-instance");
+    dn.putValue("type", "resource");
+    dn.putValue("path", "");
+    dn.putValue("optimum", 2.0);
+    dn.putValue("random", 42.0);
+    DataNode ins = new DataNode("Instances");
+    ins.putDataNode(dn);
+    
+    ProblemInfo problemInfo = new ProblemInfo("TEST");
+    problemInfo.putDataNode(ins);
 
     problemScoreCalculator = new ProblemScoreCalculator(problemInfo);
 
-    double optimum = problemInfo.getProblemInstanceInfo("uf100-0169").getValueDouble("optimum");
-    double random = problemInfo.getProblemInstanceInfo("uf100-0169").getValueDouble("random");
-    double midsection = random - optimum;
+    double optimum = problemInfo.getProblemInstanceInfo("test-instance").getValueDouble("optimum");
+    double random = problemInfo.getProblemInstanceInfo("test-instance").getValueDouble("random");
+    double midsection = (random - optimum) / 2;
 
-    assertEquals(0.5, 
-        problemScoreCalculator.calculateInstanceScore("uf100-169", midsection), 0.001);
+    assertEquals(
+        0.5, problemScoreCalculator.calculateInstanceScore("test-instance", midsection), 0.1);
   }
 
   @Test
   public void testCalculatingBetterThanOptimumInstanceScore() throws Exception {
-    Map<String, IProblemProvider<Phenotype<?>>> providers = ProblemProvider.getProblemProviders();
-    IProblemProvider<?> pp = providers.get("SAT");
-    ProblemInfo problemInfo = pp.getProblemInfo();
+    DataNode dn = new DataNode("test-instance");
+    dn.putValue("id", "test-instance");
+    dn.putValue("type", "resource");
+    dn.putValue("path", "");
+    dn.putValue("optimum", 2.0);
+    dn.putValue("random", 42.0);
+    DataNode ins = new DataNode("Instances");
+    ins.putDataNode(dn);
+    
+    ProblemInfo problemInfo = new ProblemInfo("TEST");
+    problemInfo.putDataNode(ins);
 
     problemScoreCalculator = new ProblemScoreCalculator(problemInfo);
 
-    double optimum = problemInfo.getProblemInstanceInfo("uf100-0169").getValueDouble("optimum");
+    double optimum = problemInfo.getProblemInstanceInfo("test-instance").getValueDouble("optimum");
     
     assertThrows(
         Exception.class, 
-        () -> problemScoreCalculator.calculateInstanceScore("uf100-169", optimum - 1));
+        () -> problemScoreCalculator.calculateInstanceScore("test-instance", optimum - 1));
   }
 
   @Test
   public void testCalculatingWorseThanRandomInstanceScore() throws Exception {
-    Map<String, IProblemProvider<Phenotype<?>>> providers = ProblemProvider.getProblemProviders();
-    IProblemProvider<?> pp = providers.get("SAT");
-    ProblemInfo problemInfo = pp.getProblemInfo();
+    DataNode dn = new DataNode("test-instance");
+    dn.putValue("id", "test-instance");
+    dn.putValue("type", "resource");
+    dn.putValue("path", "");
+    dn.putValue("optimum", 2.0);
+    dn.putValue("random", 42.0);
+    DataNode ins = new DataNode("Instances");
+    ins.putDataNode(dn);
+    
+    ProblemInfo problemInfo = new ProblemInfo("TEST");
+    problemInfo.putDataNode(ins);
 
     problemScoreCalculator = new ProblemScoreCalculator(problemInfo);
 
-    double random = problemInfo.getProblemInstanceInfo("uf100-0169").getValueDouble("random");
+    double random = problemInfo.getProblemInstanceInfo("test-instance").getValueDouble("random");
   
-    assertEquals(0.0, random + 1);
+    assertEquals(0.0, 
+        problemScoreCalculator.calculateInstanceScore("test-instance", random + 1), 0.1);
   }
 
   @Test
   public void testCalculatingProblemScore() throws Exception {
-    Map<String, IProblemProvider<Phenotype<?>>> providers = ProblemProvider.getProblemProviders();
-    IProblemProvider<?> pp = providers.get("SAT");
-    ProblemInfo problemInfo = pp.getProblemInfo();
-
-    problemScoreCalculator = new ProblemScoreCalculator(problemInfo);
-    
     String[] names = {"1", "2", "3", "4", "5"};
     double[] values = {0.4, 0.2, 0.1, 0.6, 0.3};
-    double meanValue = Arrays.stream(values).sum() / values.length;
+    double[] weights = {10.0, 11.0, 12.0, 13.0, 15.0};
 
-    assertEquals(meanValue, problemScoreCalculator.calculateProblemScore(names, values), 0.001);
+    DataNode ins = new DataNode("Instances");
+
+    for (int i = 0; i < names.length; i++) {
+      DataNode dn = new DataNode(names[i]);
+      dn.putValue("id", names[i]);
+      dn.putValue("type", "resource");
+      dn.putValue("path", "");
+      dn.putValue("optimum", 2.0);
+      dn.putValue("random", 42.0);
+      dn.putValue("size", weights[i]);
+
+      ins.putDataNode(dn);
+    }
+    
+    ProblemInfo problemInfo = new ProblemInfo("TEST");
+    problemInfo.putDataNode(ins);
+
+
+    problemScoreCalculator = new ProblemScoreCalculator(problemInfo);
+
+
+    int arrayLength = names.length;
+
+    double numerator = 0.0;
+    double denominator = 0.0;
+    for (int i = 0; i < arrayLength; i++) {
+      // Weight
+      double instanceSize = problemInfo
+          .getProblemInstanceInfo(names[i]).getValueDouble("size");
+      
+      numerator += instanceSize * values[i];
+      denominator += instanceSize;
+    }
+
+    double weightedMean = numerator / denominator;
+
+    assertEquals(weightedMean, problemScoreCalculator.calculateProblemScore(names, values), 0.1);
   }
 }
