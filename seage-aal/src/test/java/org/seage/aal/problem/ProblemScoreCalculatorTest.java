d016fbd3533338456c1d5649dfaf77a81c0ad98d
Richard Malek <rick@rickq.net>
2021-04-07 23:15:39 +0200

    #48 Added an empty line

--- seage-aal/src/test/java/org/seage/aal/problem/ProblemScoreCalculatorTest.java
+++ seage-aal/src/test/java/org/seage/aal/problem/ProblemScoreCalculatorTest.java
@@ -2,6 +2,7 @@ package org.seage.aal.problem;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import java.util.Arrays;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
