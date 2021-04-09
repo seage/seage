078fe9a81a33a50f25cce17bd618fc6b261373e2
David Omrai <david.omrai@seznam.cz>
2021-04-09 12:42:57 +0200

    #49 Fixed pipeline

--- seage-hyperheuristics/src/main/resources/org/seage/hh/knowledgebase/db/migration/V002__add_score_to_solutions.sql
+++ seage-hyperheuristics/src/main/resources/org/seage/hh/knowledgebase/db/migration/V002__add_score_to_solutions.sql
@@ -1,6 +1,3 @@
 -- author David Omrai
 ALTER TABLE seage.solutions
 ADD score DOUBLE PRECISION;
-
-ALTER TABLE seage.experiments
-ADD scoreCard TEXT;
---
+++ seage-hyperheuristics/src/main/resources/org/seage/hh/knowledgebase/db/migration/V003__add_score_card.sql
@@ -0,0 +1,3 @@
+-- author David Omrai
+ALTER TABLE seage.experiments
+ADD scoreCard TEXT;
