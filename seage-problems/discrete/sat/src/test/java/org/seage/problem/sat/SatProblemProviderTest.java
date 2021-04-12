88d4dafdf6c0f2107335e5d555732cd8fc01351a
David Omrai <david.omrai@seznam.cz>
2021-04-13 01:20:11 +0200

    #54 Prepared unit test, not finished

--- seage-problems/discrete/sat/src/test/java/org/seage/problem/sat/SatProblemProviderTest.java
+++ seage-problems/discrete/sat/src/test/java/org/seage/problem/sat/SatProblemProviderTest.java
@@ -1,9 +1,35 @@
 package org.seage.problem.sat;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.List;
+import java.util.Random;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.seage.aal.problem.IProblemProvider;
+import org.seage.aal.problem.ProblemInstance;
+import org.seage.aal.problem.ProblemInstanceInfo;
+import org.seage.aal.problem.ProblemProvider;
 import org.seage.aal.problem.ProblemProviderTestBase;
+import org.seage.aal.problem.ProblemInstanceInfo.ProblemInstanceOrigin;
 
-public class SatProblemProviderTest extends ProblemProviderTestBase<SatPhenotype> {
-  public SatProblemProviderTest() {
-    super(new SatProblemProvider());
+public class SatProblemProviderTest {
+  SatProblemProvider problemProvider;
+
+  @BeforeAll
+  public void init() {
+    problemProvider = new SatProblemProvider();
+  }
+
+  @Test
+  public void generateGreedySolutionTest() throws Exception {
+    Random rnd = new Random(10000);
+    
+    IProblemProvider provider = ProblemProvider.getProblemProviders().get("SAT");
+    // problem instance
+    ProblemInstance instance = provider
+        .initProblemInstance(provider.getProblemInfo().getProblemInstanceInfo("uf20-01"));
+
+        
+    assertNotNull(problemProvider.generateGreedySolution(instance, rnd.nextLong()));
   }
 }
