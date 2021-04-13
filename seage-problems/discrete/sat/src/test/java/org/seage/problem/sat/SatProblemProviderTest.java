354c96af7ffeec36bda17f415dcda5cc8d409725
David Omrai <david.omrai@seznam.cz>
2021-04-13 09:55:29 +0200

    #54 Fixed broken test

--- seage-problems/discrete/sat/src/test/java/org/seage/problem/sat/SatProblemProviderTest.java
+++ seage-problems/discrete/sat/src/test/java/org/seage/problem/sat/SatProblemProviderTest.java
@@ -1,30 +1,29 @@
 package org.seage.problem.sat;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import java.util.List;
+
 import java.util.Random;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.seage.aal.algorithm.Phenotype;
 import org.seage.aal.problem.IProblemProvider;
 import org.seage.aal.problem.ProblemInstance;
-import org.seage.aal.problem.ProblemInstanceInfo;
 import org.seage.aal.problem.ProblemProvider;
-import org.seage.aal.problem.ProblemProviderTestBase;
-import org.seage.aal.problem.ProblemInstanceInfo.ProblemInstanceOrigin;
 
 public class SatProblemProviderTest {
-  SatProblemProvider problemProvider;
+  static SatProblemProvider problemProvider;
 
   @BeforeAll
-  public void init() {
+  public static void init() {
     problemProvider = new SatProblemProvider();
   }
 
   @Test
   public void generateGreedySolutionTest() throws Exception {
     Random rnd = new Random(10000);
-    
-    IProblemProvider provider = ProblemProvider.getProblemProviders().get("SAT");
+
+    ProblemProvider.registerProblemProviders(new Class<?>[] { SatProblemProvider.class });
+    IProblemProvider<Phenotype<?>> provider = ProblemProvider.getProblemProviders().get("SAT");
     // problem instance
     ProblemInstance instance = provider
         .initProblemInstance(provider.getProblemInfo().getProblemInstanceInfo("uf20-01"));
