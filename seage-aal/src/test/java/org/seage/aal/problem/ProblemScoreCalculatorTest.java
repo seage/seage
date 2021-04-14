d7214797510df4dc83ae0a891b55e127359e16e5
Richard Malek <rick@rickq.net>
2021-04-14 19:54:13 +0200

    #56 Formated the test code

--- seage-aal/src/test/java/org/seage/aal/problem/ProblemScoreCalculatorTest.java
+++ seage-aal/src/test/java/org/seage/aal/problem/ProblemScoreCalculatorTest.java
@@ -12,12 +12,11 @@ import org.seage.data.DataNode;
 public class ProblemScoreCalculatorTest {
   protected ProblemScoreCalculator problemScoreCalculator;
   protected static ProblemInfo singleProblemInfo;
-  
+
 
   @BeforeAll
   static void init() throws Exception {
-    ProblemProvider.registerProblemProviders(
-        new Class<?>[] {TestProblemProvider.class});
+    ProblemProvider.registerProblemProviders(new Class<?>[] {TestProblemProvider.class});
 
     DataNode dn = new DataNode("test-instance");
     dn.putValue("id", "test-instance");
@@ -28,7 +27,7 @@ public class ProblemScoreCalculatorTest {
     dn.putValue("greedy", 42.0);
     DataNode ins = new DataNode("Instances");
     ins.putDataNode(dn);
-    
+
     singleProblemInfo = new ProblemInfo("TEST");
     singleProblemInfo.putDataNode(ins);
   }
@@ -38,24 +37,22 @@ public class ProblemScoreCalculatorTest {
   public void testCalculatingOptimalInstanceScore() throws Exception {
     problemScoreCalculator = new ProblemScoreCalculator(singleProblemInfo);
 
-    double optimum = singleProblemInfo
-        .getProblemInstanceInfo("test-instance").getValueDouble("optimum");
-   
-    assertEquals(
-        1.0, problemScoreCalculator.calculateInstanceScore("test-instance", optimum), 0.1);
+    double optimum =
+        singleProblemInfo.getProblemInstanceInfo("test-instance").getValueDouble("optimum");
+
+    assertEquals(1.0, problemScoreCalculator.calculateInstanceScore("test-instance", optimum), 0.1);
   }
 
 
   @Test
   public void testCalculatingGreedyInstanceScore() throws Exception {
-    
+
     problemScoreCalculator = new ProblemScoreCalculator(singleProblemInfo);
 
-    double greedy = singleProblemInfo
-        .getProblemInstanceInfo("test-instance").getValueDouble("greedy");
-    
-    assertEquals(
-        0.0, problemScoreCalculator.calculateInstanceScore("test-instance", greedy), 0.1);
+    double greedy =
+        singleProblemInfo.getProblemInstanceInfo("test-instance").getValueDouble("greedy");
+
+    assertEquals(0.0, problemScoreCalculator.calculateInstanceScore("test-instance", greedy), 0.1);
   }
 
 
@@ -63,14 +60,14 @@ public class ProblemScoreCalculatorTest {
   public void testCalculatingMidsectionInstanceScore() throws Exception {
     problemScoreCalculator = new ProblemScoreCalculator(singleProblemInfo);
 
-    double optimum = singleProblemInfo
-        .getProblemInstanceInfo("test-instance").getValueDouble("optimum");
-    double greedy = singleProblemInfo
-        .getProblemInstanceInfo("test-instance").getValueDouble("greedy");
+    double optimum =
+        singleProblemInfo.getProblemInstanceInfo("test-instance").getValueDouble("optimum");
+    double greedy =
+        singleProblemInfo.getProblemInstanceInfo("test-instance").getValueDouble("greedy");
     double midsection = (greedy - optimum) / 2;
 
-    assertEquals(
-        0.5, problemScoreCalculator.calculateInstanceScore("test-instance", midsection), 0.1);
+    assertEquals(0.5, problemScoreCalculator.calculateInstanceScore("test-instance", midsection),
+        0.1);
   }
 
 
@@ -78,11 +75,10 @@ public class ProblemScoreCalculatorTest {
   public void testCalculatingBetterThanOptimumInstanceScore() throws Exception {
     problemScoreCalculator = new ProblemScoreCalculator(singleProblemInfo);
 
-    double optimum = singleProblemInfo
-        .getProblemInstanceInfo("test-instance").getValueDouble("optimum");
-    
-    assertThrows(
-        Exception.class, 
+    double optimum =
+        singleProblemInfo.getProblemInstanceInfo("test-instance").getValueDouble("optimum");
+
+    assertThrows(Exception.class,
         () -> problemScoreCalculator.calculateInstanceScore("test-instance", optimum - 1));
   }
 
@@ -91,11 +87,11 @@ public class ProblemScoreCalculatorTest {
   public void testCalculatingWorseThanGreedyInstanceScore() throws Exception {
     problemScoreCalculator = new ProblemScoreCalculator(singleProblemInfo);
 
-    double greedy = singleProblemInfo
-        .getProblemInstanceInfo("test-instance").getValueDouble("greedy");
-  
-    assertEquals(0.0, 
-        problemScoreCalculator.calculateInstanceScore("test-instance", greedy + 1), 0.1);
+    double greedy =
+        singleProblemInfo.getProblemInstanceInfo("test-instance").getValueDouble("greedy");
+
+    assertEquals(0.0, problemScoreCalculator.calculateInstanceScore("test-instance", greedy + 1),
+        0.1);
   }
 
 
@@ -119,7 +115,7 @@ public class ProblemScoreCalculatorTest {
 
       ins.putDataNode(dn);
     }
-    
+
     ProblemInfo problemInfo = new ProblemInfo("TEST");
     problemInfo.putDataNode(ins);
     problemScoreCalculator = new ProblemScoreCalculator(problemInfo);
@@ -131,17 +127,17 @@ public class ProblemScoreCalculatorTest {
 
     for (int i = 0; i < arrayLength; i++) {
       // Weight
-      double instanceSize = problemInfo
-          .getProblemInstanceInfo(instanceIDs[i]).getValueDouble("size");
-      
+      double instanceSize =
+          problemInfo.getProblemInstanceInfo(instanceIDs[i]).getValueDouble("size");
+
       numerator += instanceSize * objValues[i];
       denominator += instanceSize;
     }
 
     double weightedMean = numerator / denominator;
 
-    assertEquals(
-        weightedMean, problemScoreCalculator.calculateProblemScore(instanceIDs, objValues), 0.1);
+    assertEquals(weightedMean, problemScoreCalculator.calculateProblemScore(instanceIDs, objValues),
+        0.1);
   }
 
 
@@ -164,16 +160,15 @@ public class ProblemScoreCalculatorTest {
 
       ins.putDataNode(dn);
     }
-    
+
     ProblemInfo problemInfo = new ProblemInfo("TEST");
     problemInfo.putDataNode(ins);
     problemScoreCalculator = new ProblemScoreCalculator(problemInfo);
 
-    
+
     double[] objValues = {0.4, 0.2, 0.1, 0.6, 0.3};
     double mean = Arrays.stream(objValues).sum() / objValues.length;
 
-    assertEquals(
-        mean, problemScoreCalculator.calculateProblemScore(instanceIDs, objValues), 0.1);
+    assertEquals(mean, problemScoreCalculator.calculateProblemScore(instanceIDs, objValues), 0.1);
   }
 }
