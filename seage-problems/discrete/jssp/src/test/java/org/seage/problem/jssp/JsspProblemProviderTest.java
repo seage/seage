0309f7a8cddc6fb42c0f0829ac904253c4ab6811
David Omrai <david.omrai@seznam.cz>
2021-10-18 22:50:36 +0200

    #59 Playing with tests

--- seage-problems/discrete/jssp/src/test/java/org/seage/problem/jssp/JsspProblemProviderTest.java
+++ seage-problems/discrete/jssp/src/test/java/org/seage/problem/jssp/JsspProblemProviderTest.java
@@ -1,11 +1,10 @@
 package org.seage.problem.jssp;
 
-import org.seage.aal.problem._ProblemProviderTestBase;
+import org.seage.aal.problem.ProblemProviderTestBase;
 
 // @Ignore
-public class JsspProblemProviderTest extends _ProblemProviderTestBase
+public class JsspProblemProviderTest extends ProblemProviderTestBase
 {
-
     public JsspProblemProviderTest()
     {
         super(new JsspProblemProvider());
