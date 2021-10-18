d2f8c4b760bd6f07f0a8023e0faa0f042e2355cc
David Omrai <david.omrai@seznam.cz>
2021-10-18 19:39:10 +0200

    #59 Added some documentation to jssp phenotype, info about what the integer array means

--- seage-problems/discrete/jssp/src/main/java/org/seage/problem/jssp/JsspPhenotype.java
+++ seage-problems/discrete/jssp/src/main/java/org/seage/problem/jssp/JsspPhenotype.java
@@ -1,3 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Richard Malek and SEAGE contributors
+ * 
+ * This file is part of SEAGE.
+ * 
+ * SEAGE is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * SEAGE is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * SEAGE. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/**
+ * Contributors: Richard Malek - Initial implementation, David Omrai - Adapting to JSSP problem
+ */
 package org.seage.problem.jssp;
 
 import java.util.Arrays;
@@ -6,6 +28,13 @@ import org.seage.aal.algorithm.Phenotype;
 
 public class JsspPhenotype extends Phenotype<Integer[]> {
 
+  /**
+   * Class constructor sets the solution
+   * Solution is the integer array where each number represents
+   * the job operation for one working machine
+   * @param schedule Integer[] solution
+   * @throws Exception In case of wrong input
+   */
   public JsspPhenotype(Integer[] schedule) throws Exception {
     super(schedule);
   }
