282a87419080b21ee60cfd59ba09dfb2ba6f6f85
David Omrai <david.omrai@seznam.cz>
2021-10-24 16:12:00 +0200

    #60 Added some code for greedy algorithm for jssp problem

--- seage-problems/discrete/jssp/src/main/java/org/seage/problem/jssp/ScheduleProvider.java
+++ seage-problems/discrete/jssp/src/main/java/org/seage/problem/jssp/ScheduleProvider.java
@@ -19,6 +19,63 @@ public class ScheduleProvider {
    * @param randomSeed Random seed.
    */
   public static JsspPhenotype createGreedySchedule(JobsDefinition jobs, long randomSeed) throws Exception {
+    JobInfo[] jobInfos = jobs.getJobInfos();
+
+    int _numJobs = jobs.getJobsCount();
+    int _numMachines = jobInfos[0].getOperationInfos().length;
+
+    int[] greedySolution = new int[_numJobs*_numMachines];
+
+    int[] _lastActivityInJobIndex = new int[_numJobs];
+    int[] _lastActivityOnMachineIndex = new int[_numMachines];
+
+    int[] _endTimeInJob = new int[_numJobs];
+    int[] _endTimeOnMachine = new int[_numMachines];
+
+    int indexCurrentMachine = 0;
+    int indexCurrentJob = 0;
+    int indexCurrentOper = 0;
+
+    int maxMakeSpan = 0;
+    int tmpMakeSpan = 0;
+    int tmpMinMakeSpan = 0;
+
+    JobInfo currentJob;
+    OperationInfo currentOper;
+
+    int nextJobId;
+
+    for (int i = 0; i < greedySolution.length; i++) {
+      
+      nextJobId = 0;
+      for (int jobId = 1; jobId <= _numJobs; jobId++) {
+        indexCurrentJob = jobId - 1;
+        indexCurrentOper = _lastActivityInJobIndex[indexCurrentJob];
+
+        currentJob = jobs.getJobInfos()[indexCurrentJob];
+        if ( indexCurrentJob >= currentJob.getOperationInfos().length)
+          continue;
+        currentOper = currentJob.getOperationInfos()[indexCurrentOper];
+
+        indexCurrentMachine = currentOper.MachineID - 1;
+
+        if (_endTimeOnMachine[indexCurrentMachine] > _endTimeInJob[indexCurrentJob]) {
+          tmpMakeSpan = _endTimeOnMachine[indexCurrentJob] + currentOper.Length;
+        }
+        else {
+          tmpMakeSpan = _endTimeInJob[indexCurrentJob] + currentOper.Length;
+        }
+
+        if (tmpMinMakeSpan == 0 || tmpMinMakeSpan > tmpMakeSpan) {
+          tmpMinMakeSpan = tmpMakeSpan;
+          nextJobId = jobId;
+        }
+      }
+
+      // Strore the next new solution
+      greedySolution[i] = nextJobId;
+    }
+    
     return null;
   }
 
