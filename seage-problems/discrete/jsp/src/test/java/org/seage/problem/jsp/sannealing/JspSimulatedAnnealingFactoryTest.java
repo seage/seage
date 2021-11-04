96585757bbeed5a476f39efac90acac7ced3924d
David Omrai <david.omrai@seznam.cz>
2021-11-04 17:59:11 +0100

    #64 Changed the status from disabled to testable

--- seage-problems/discrete/jsp/src/test/java/org/seage/problem/jsp/sannealing/JspSimulatedAnnealingFactoryTest.java
+++ seage-problems/discrete/jsp/src/test/java/org/seage/problem/jsp/sannealing/JspSimulatedAnnealingFactoryTest.java
@@ -1,11 +1,11 @@
 package org.seage.problem.jsp.sannealing;
 
-import org.junit.jupiter.api.Disabled;
+import org.junit.platform.commons.annotation.Testable;
 import org.seage.aal.problem.ProblemAlgorithmAdapterTestBase;
 import org.seage.problem.jsp.JspPhenotype;
 import org.seage.problem.jsp.JspProblemProvider;
 
-@Disabled
+@Testable
 public class JspSimulatedAnnealingFactoryTest extends ProblemAlgorithmAdapterTestBase<JspPhenotype>
 {
     public JspSimulatedAnnealingFactoryTest()
