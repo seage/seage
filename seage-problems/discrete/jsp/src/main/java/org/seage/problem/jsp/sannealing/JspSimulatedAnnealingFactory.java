bcf802540a4e799b98669faf8d977fe9b55b7880
Richard Malek <rick@rickq.net>
2021-11-08 22:34:52 +0100

    #64 Fixed tab indetation

--- seage-problems/discrete/jsp/src/main/java/org/seage/problem/jsp/sannealing/JspSimulatedAnnealingFactory.java
+++ seage-problems/discrete/jsp/src/main/java/org/seage/problem/jsp/sannealing/JspSimulatedAnnealingFactory.java
@@ -1,31 +1,24 @@
 /*******************************************************************************
  * Copyright (c) 2009 Richard Malek and SEAGE contributors
-
- * This file is part of SEAGE. 
-
- * SEAGE is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
-
- * SEAGE is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
-
- * You should have received a copy of the GNU General Public License
- * along with SEAGE. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * This file is part of SEAGE.
+ * 
+ * SEAGE is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * SEAGE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with SEAGE. If not, see
+ * <http://www.gnu.org/licenses/>.
  *
  */
 
 /**
- * Contributors:
- *   Jan Zmatlik
- *   - Initial implementation
- *   Richard Malek
- *   - Added algorithm annotations
- *   David Omrai
- *   - Editation and debugging
+ * Contributors: Jan Zmatlik - Initial implementation Richard Malek - Added algorithm annotations
+ * David Omrai - Editation and debugging
  */
 package org.seage.problem.jsp.sannealing;
 
@@ -39,41 +32,39 @@ import org.seage.problem.jsp.JobsDefinition;
 import org.seage.problem.jsp.JspPhenotype;
 import org.seage.problem.jsp.JspPhenotypeEvaluator;
 import org.seage.problem.jsp.JspProblemProvider;
+
 /**
  *
  * @author Jan Zmatlik
  */
 @Annotations.AlgorithmId("SimulatedAnnealing")
 @Annotations.AlgorithmName("Simulated Annealing")
-public class JspSimulatedAnnealingFactory implements IAlgorithmFactory<JspPhenotype, JspSimulatedAnnealingSolution>
-{
+public class JspSimulatedAnnealingFactory
+    implements IAlgorithmFactory<JspPhenotype, JspSimulatedAnnealingSolution> {
   @Override
-  public Class<?> getAlgorithmClass()
-  {
+  public Class<?> getAlgorithmClass() {
     return SimulatedAnnealingAdapter.class;
   }
 
   @Override
-  public IAlgorithmAdapter<JspPhenotype, JspSimulatedAnnealingSolution> createAlgorithm(ProblemInstance instance,
-    IPhenotypeEvaluator<JspPhenotype> phenotypeEvaluator) throws Exception {
+  public IAlgorithmAdapter<JspPhenotype, JspSimulatedAnnealingSolution> createAlgorithm(
+      ProblemInstance instance, IPhenotypeEvaluator<JspPhenotype> phenotypeEvaluator)
+      throws Exception {
     JspProblemProvider problemProvider = new JspProblemProvider();
     JspPhenotypeEvaluator evaluator =
         new JspPhenotypeEvaluator(problemProvider.getProblemInfo(), (JobsDefinition) instance);
     JspObjectiveFunction objFun = new JspObjectiveFunction(evaluator);
-  	return new SimulatedAnnealingAdapter<JspPhenotype, JspSimulatedAnnealingSolution>(
-      objFun, new JspMoveManager((JobsDefinition) instance, objFun), phenotypeEvaluator, false)
-    {
+    return new SimulatedAnnealingAdapter<JspPhenotype, JspSimulatedAnnealingSolution>(objFun,
+        new JspMoveManager((JobsDefinition) instance, objFun), phenotypeEvaluator, false) {
       @Override
-      public void solutionsFromPhenotype(JspPhenotype[] source) throws Exception
-      {
+      public void solutionsFromPhenotype(JspPhenotype[] source) throws Exception {
         this.solutions = new JspSimulatedAnnealingSolution[source.length];
         for (int i = 0; i < this.solutions.length; i++)
           this.solutions[i] = new JspSimulatedAnnealingSolution(source[i].getSolution());
       }
 
       @Override
-      public JspPhenotype[] solutionsToPhenotype() throws Exception
-      {
+      public JspPhenotype[] solutionsToPhenotype() throws Exception {
         JspPhenotype[] result = new JspPhenotype[this.solutions.length];
         for (int i = 0; i < this.solutions.length; i++) {
           result[i] = solutionToPhenotype(this.solutions[i]);
@@ -81,14 +72,15 @@ public class JspSimulatedAnnealingFactory implements IAlgorithmFactory<JspPhenot
         return result;
       }
 
-			@Override
-			public JspPhenotype solutionToPhenotype(JspSimulatedAnnealingSolution solution) throws Exception {
-				JspPhenotype result = new JspPhenotype(solution.getJobArray());
+      @Override
+      public JspPhenotype solutionToPhenotype(JspSimulatedAnnealingSolution solution)
+          throws Exception {
+        JspPhenotype result = new JspPhenotype(solution.getJobArray());
         double[] objVals = this.phenotypeEvaluator.evaluate(result);
         result.setObjValue(objVals[0]);
         result.setScore(objVals[1]);
         return result;
-			}
+      }
     };
   }
 
