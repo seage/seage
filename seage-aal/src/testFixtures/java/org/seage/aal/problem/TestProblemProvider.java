2e25ea2f53a6ec88fa6f918c9d80c5542031002f
Richard Malek <rick@rickq.net>
2022-03-12 16:37:33 +0100

    #69 Removed some comments

--- seage-aal/src/testFixtures/java/org/seage/aal/problem/TestProblemProvider.java
+++ seage-aal/src/testFixtures/java/org/seage/aal/problem/TestProblemProvider.java
@@ -9,40 +9,33 @@ import org.seage.aal.algorithm.TestPhenotype;
 public class TestProblemProvider extends ProblemProvider<TestPhenotype> {
 
   public TestProblemProvider() {
-
   }
 
   @Override
   public ProblemInstance initProblemInstance(ProblemInstanceInfo problemInstanceInfo)
       throws Exception {
-    // TODO Auto-generated method stub
     return null;
   }
 
   @Override
   public IPhenotypeEvaluator<TestPhenotype> initPhenotypeEvaluator(ProblemInstance problemInstance)
       throws Exception {
-    // TODO Auto-generated method stub
     return null;
   }
 
   @Override
   public TestPhenotype[] generateInitialSolutions(ProblemInstance problemInstance, int numSolutions,
       long randomSeed) throws Exception {
-    // TODO Auto-generated method stub
     return null;
   }
 
   @Override
   public void visualizeSolution(Object[] solution, ProblemInstanceInfo problemInstanceInfo)
       throws Exception {
-    // TODO Auto-generated method stub
-
   }
 
   @Override
   public ProblemMetadataGenerator<TestPhenotype> initProblemMetadataGenerator() {
-    // TODO Auto-generated method stub
     return null;
   }
 }
