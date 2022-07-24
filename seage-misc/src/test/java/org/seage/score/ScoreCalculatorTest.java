f3c1ce67d34c4693cb256c075be87dcf378d3ebb
David Omrai <david.omrai@seznam.cz>
2022-07-24 14:05:54 +0200

    #81 Finished the rest of test and changed names of numerator and denumerator

--- seage-misc/src/test/java/org/seage/score/ScoreCalculatorTest.java
+++ seage-misc/src/test/java/org/seage/score/ScoreCalculatorTest.java
@@ -10,6 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -59,17 +60,67 @@ public class ScoreCalculatorTest {
       }
     };
 
-    double numerator = 0.0;
-    double denominator = 0.0;
+    double weightedScoresSum = 0.0;
+    double insSizesSum = 0.0;
 
     for (int i = 0; i < instanceSizes.size(); i++) {
-      numerator += instanceSizes.get(i) * instanceScores[i];
-      denominator += instanceSizes.get(i);
+      weightedScoresSum += instanceSizes.get(i) * instanceScores[i];
+      insSizesSum += instanceSizes.get(i);
     }
 
-    double weightedMean = numerator / denominator;
+    double weightedMean = weightedScoresSum / insSizesSum;
 
     assertEquals(weightedMean, 
         ScoreCalculator.calculateProblemScore(instanceSizes, instanceScores));
+  }
+
+  @Test
+  public void testCalculatingBasicMeanOfScores() throws Exception {
+    double[] instanceScores = {0.4, 0.2, 0.1, 0.6, 0.3};    
+    List<Double> instanceSizes =  new ArrayList<>() {{
+        add(1.0);
+        add(1.0); 
+        add(1.0);
+        add(1.0); 
+        add(1.0);
+      }
+    };
+    double mean = Arrays.stream(instanceScores).sum() / instanceScores.length;
+
+    assertEquals(mean, ScoreCalculator.calculateProblemScore(instanceSizes, instanceScores));
+  }
+
+  @Test
+  public void testCalculatingProblemScoreException() throws Exception {
+    double[] instanceScores = {0.0, 0.0, 0.0, 0.0, 0.0};    
+    List<Double> instanceSizes =  new ArrayList<>() {{
+        add(0.0);
+        add(0.0); 
+        add(0.0);
+        add(0.0); 
+        add(0.0);
+      }
+    };
+
+    assertThrows(ArithmeticException.class,
+        () -> ScoreCalculator.calculateProblemScore(instanceSizes, instanceScores));
+  
+  }
+
+  @Test
+  public void testCalculateExperimentScore() throws Exception {
+    List<Double> problemsScores = new ArrayList<>() {{
+        add(10.0);
+        add(5.0);
+        add(3.0);
+      }
+    };
+
+    assertEquals(6.0, ScoreCalculator.calculateExperimentScore(problemsScores), 0.1);
+  }
+
+  @Test
+  public void testCalculateScoreDelta() throws Exception {
+    assertEquals(6.0, ScoreCalculator.calculateScoreDelta(12.0, 6.0), 0.1);
   }
 }
