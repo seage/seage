e867108083cfbf75b1ede2911fa95da4dbe90a7d
Richard Malek <rick@rickq.net>
2022-08-04 00:29:06 +0200

    #18 Fixed some lint issues

--- seage-launcher/src/main/java/org/seage/launcher/commands/ProblemInstanceParamsParser.java
+++ seage-launcher/src/main/java/org/seage/launcher/commands/ProblemInstanceParamsParser.java
@@ -7,39 +7,33 @@ import java.util.List;
 import java.util.Map;
 
 public class ProblemInstanceParamsParser {
-  // HyFlex instancesIDs
-  public static final Map<String, List<String>> hyflexInstances = new HashMap<>() {
-    {
-      put("SAT", new ArrayList<>(Arrays.asList(
-        "pg-525-2276-hyflex-3",
-        "pg-696-3122-hyflex-5", 
-        "pg-525-2336-hyflex-4",
-        "jarv-684-2300-hyflex-10", 
-        "hg4-300-1200-hyflex-11"
-      )));
-      put("TSP", new ArrayList<>(Arrays.asList(
-        "pr299-hyflex-0", 
-        "usa13509-hyflex-8", 
-        "rat575-hyflex-2",
-        "u2152-hyflex-7", 
-        "d1291-hyflex-6"
-      )));
-      put("FSP", new ArrayList<>(Arrays.asList(
-        "tai100_20_02",
-        "tai500_20_02",
-        "tai100_20_04",
-        "tai200_20_01",
-        "tai500_20_03"
-      )));
-      put("QAP", new ArrayList<>(Arrays.asList(
-        "sko100a",
-        "tai100a",
-        "tai256c",
-        "tho150",
-        "wil100"
-      )));
-    }};
 
+  private static final Map<String,  List<String>> hyflexInstances = Map.of(
+      "SAT", Arrays.asList(
+          "pg-525-2276-hyflex-3",
+          "pg-696-3122-hyflex-5", 
+          "pg-525-2336-hyflex-4",
+          "jarv-684-2300-hyflex-10", 
+          "hg4-300-1200-hyflex-11"),
+      "TSP", Arrays.asList(
+          "pr299-hyflex-0", 
+          "usa13509-hyflex-8", 
+          "rat575-hyflex-2",
+          "u2152-hyflex-7", 
+          "d1291-hyflex-6"),
+      "FSP", Arrays.asList(
+          "tai100_20_02",
+          "tai500_20_02",
+          "tai100_20_04",
+          "tai200_20_01",
+          "tai500_20_03"),
+      "QAP", Arrays.asList(
+          "sko100a",
+          "tai100a",
+          "tai256c",
+          "tho150",
+          "wil100") 
+  );
 
   /**
    * Parses command line instance arguments.
@@ -47,7 +41,7 @@ public class ProblemInstanceParamsParser {
    * @return Map of problems and instances.
    * @throws Exception .
    */
-  public static HashMap<String, List<String>> parseProblemInstanceParams(
+  public static Map<String, List<String>> parseProblemInstanceParams(
       List<String> instanceParams) throws Exception {
     
     HashMap<String, List<String>> instances = new HashMap<>();
