6ac6abf19a14309de1e969a1340d37b39c4c849e
David Omrai <david.omrai@seznam.cz>
2022-08-25 19:31:04 +0200

    #93 Bug fix

--- seage-hyperheuristics/src/main/java/org/seage/hh/experimenter/singlealgorithm/SingleAlgorithmDefaultExperimenter.java
+++ seage-hyperheuristics/src/main/java/org/seage/hh/experimenter/singlealgorithm/SingleAlgorithmDefaultExperimenter.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import org.seage.hh.experimenter.configurator.DefaultConfigurator;
-import com.google.common.collect.Multiset.Entry;
 
 public class SingleAlgorithmDefaultExperimenter extends SingleAlgorithmExperimenter {
 
