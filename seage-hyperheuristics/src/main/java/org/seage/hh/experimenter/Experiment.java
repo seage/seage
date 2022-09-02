60342284518234d81a2b5e5e6c27629bd3973867
David Omrai <david.omrai@seznam.cz>
2022-09-02 13:50:32 +0200

    #93 Removed logger from interface

--- seage-hyperheuristics/src/main/java/org/seage/hh/experimenter/Experiment.java
+++ seage-hyperheuristics/src/main/java/org/seage/hh/experimenter/Experiment.java
@@ -1,10 +1,5 @@
 package org.seage.hh.experimenter;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 public interface Experiment {
-  static Logger logger =
-      LoggerFactory.getLogger(Experimenter.class.getName());
-
   Double run() throws Exception;
 }
