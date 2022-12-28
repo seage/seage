2f928153d9c39e20d30846ce5ab1cce2da16fb7b
David Omrai <david.omrai@seznam.cz>
2022-12-28 16:45:47 +0100

    #30 Wrote a unit test, but there's a problem with data in db, after commit none stays there

--- seage-hyperheuristics/src/test/java/org/seage/hh/experimenter/singlealgorithm/feedback/NewFeedbackConfiguratorTest.java
+++ seage-hyperheuristics/src/test/java/org/seage/hh/experimenter/singlealgorithm/feedback/NewFeedbackConfiguratorTest.java
@@ -1,21 +1,120 @@
 package org.seage.hh.experimenter.singlealgorithm.feedback;
 
-import org.junit.jupiter.api.Test;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.List;
-import org.seage.hh.knowledgebase.db.DbManager;
+import java.util.Date;
+import java.util.UUID;
+import org.apache.ibatis.session.SqlSession;
+import org.seage.data.DataNode;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.seage.aal.algorithm.AlgorithmParams;
+import org.seage.aal.problem.ProblemConfig;
+import org.seage.aal.problem.ProblemInfo;
+import org.seage.hh.experimenter.ExperimentTaskRequest;
 import org.seage.hh.experimenter.configurator.FeedbackConfigurator;
+import org.seage.hh.knowledgebase.db.DbManager;
+import org.seage.hh.knowledgebase.db.dbo.ExperimentRecord;
 import org.seage.hh.knowledgebase.db.dbo.ExperimentTaskRecord;
+import org.seage.hh.knowledgebase.db.mapper.ExperimentMapper;
+import org.seage.hh.knowledgebase.db.mapper.ExperimentTaskMapper;
+
 
 
 public class NewFeedbackConfiguratorTest {
+  private ExperimentRecord experiment1;
+  private ExperimentTaskRecord experimentTask1;
+  private ExperimentTaskRecord experimentTask2;
+  private ProblemInfo pi;
+
+  @BeforeEach
+  void setUp() throws Exception {
+    DbManager.initTest();
+
+    this.experiment1 = new ExperimentRecord(
+      UUID.fromString("16578d4d-9ae4-4b3f-bcf3-7e7ce4737204"), 
+      "experimentType1", 
+      "TEST", 
+      "instanceID1", 
+      "algorithmID1", 
+      "config1", 
+      new Date(), 
+      new Date(), 
+      1.0,
+      "hostname1",
+      "1"
+    );
+
+    this.experimentTask1 = new ExperimentTaskRecord(new ExperimentTaskRequest(
+      UUID.randomUUID(),
+      UUID.fromString("16578d4d-9ae4-4b3f-bcf3-7e7ce4737204"),
+      1, 1,
+      "TEST", 
+      "instanceID1", 
+      "algorithmID1", 
+      new AlgorithmParams(),
+      null,
+      1L
+    ));
+    
+    this.experimentTask2 = new ExperimentTaskRecord(new ExperimentTaskRequest(
+      UUID.randomUUID(),
+      UUID.fromString("16578d4d-9ae4-4b3f-bcf3-7e7ce4737204"), 
+      2, 2,
+      "TEST", 
+      "Instance01", 
+      "Algorithm01", 
+      new AlgorithmParams(),
+      null,
+      2L
+    ));
+
+    this.pi = new ProblemInfo("TestProblem");
+    pi.putValue("id", "TestProblem");
+    pi.putDataNode(new DataNode("Instances"));
+    pi.putDataNode(new DataNode("Algorithms"));
+
+    DataNode ii = new DataNode("Instance");
+    ii.putValue("id", "Instance01");
+    ii.putValue("name", "Instance01");
+    ii.putValue("type", "");
+    ii.putValue("path", "");
+    pi.getDataNode("Instances").putDataNode(ii);
+
+    DataNode ai = new DataNode("Algorithm");
+    ai.putValue("id", "Algorithm01");
+    pi.getDataNode("Algorithms").putDataNode(ai);
+
+  }
 
   @Test
-  public void testDb() throws Exception {
-    DbManager.init();
+  void testPrepareConfigs() throws Exception {
+    try (SqlSession session = DbManager.getSqlSessionFactory().openSession()) {
+      ExperimentMapper mapper0 = session.getMapper(ExperimentMapper.class);
+      mapper0.insertExperiment(this.experiment1);
 
-    FeedbackConfigurator nfc = new FeedbackConfigurator();
+      ExperimentTaskMapper mapper = session.getMapper(ExperimentTaskMapper.class);
+      assertNotNull(mapper);
 
-    List<ExperimentTaskRecord> output = nfc.getBestExperimentTasks("TSP", "TabuSearch", 10);
-    System.out.println(output);
+      mapper.insertExperimentTask(this.experimentTask1);
+      session.commit();
+
+      
+      ExperimentTaskRecord experimentTask = 
+          mapper.getExperimentTask(this.experimentTask1.getExperimentTaskID());      
+      assertNotNull(experimentTask);
+      assertEquals(
+          this.experimentTask1.getExperimentTaskID(), experimentTask.getExperimentTaskID());
+    }
+
+    FeedbackConfigurator fc = new FeedbackConfigurator();
+
+    ProblemConfig[] pc = fc.prepareConfigs(pi, "Instance01", "Algorithm01", 1);
+
+    assertTrue(pc.length == 1);
   }
 }
