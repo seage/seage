aa5f10464066e12d39d40e1faeddcb87376bc5e2
Richard Malek <rick@rickq.net>
2023-02-26 12:47:26 +0100

    #98 Fixed TabuSearch for FSP

--- seage-problems/discrete/fsp/src/main/java/org/seage/problem/fsp/tabusearch/FspTabuSearchFactory.java
+++ seage-problems/discrete/fsp/src/main/java/org/seage/problem/fsp/tabusearch/FspTabuSearchFactory.java
@@ -1,32 +1,38 @@
 /*******************************************************************************
  * Copyright (c) 2009 Richard Malek and SEAGE contributors
-
+ * 
  * This file is part of SEAGE.
-
- * SEAGE is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
-
- * SEAGE is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
-
- * You should have received a copy of the GNU General Public License
- * along with SEAGE. If not, @see <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.
+ * 
+ * SEAGE is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * SEAGE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with SEAGE. If not, see
+ * <http://www.gnu.org/licenses/>.
  *
  */
 
 /**
- * Contributors:
- *     Richard Malek
- *     - Initial implementation
+ * Contributors: Richard Malek - Initial implementation David Omrai - Following implementation
  */
 package org.seage.problem.fsp.tabusearch;
 
 import org.seage.aal.Annotations;
-import org.seage.problem.jsp.tabusearch.JspTabuSearchFactory;
+import org.seage.aal.algorithm.IAlgorithmAdapter;
+import org.seage.aal.algorithm.IAlgorithmFactory;
+import org.seage.aal.algorithm.IPhenotypeEvaluator;
+import org.seage.aal.algorithm.tabusearch.TabuSearchAdapter;
+import org.seage.aal.problem.ProblemInstance;
+import org.seage.problem.fsp.FspPhenotypeEvaluator;
+import org.seage.problem.jsp.JspJobsDefinition;
+import org.seage.problem.jsp.JspPhenotype;
+import org.seage.problem.jsp.tabusearch.JspTabuSearchAdapter;
+import org.seage.problem.jsp.tabusearch.JspTabuSearchSolution;
+
 
 /**
  *
@@ -34,5 +40,20 @@ import org.seage.problem.jsp.tabusearch.JspTabuSearchFactory;
  */
 @Annotations.AlgorithmId("TabuSearch")
 @Annotations.AlgorithmName("Tabu Search")
-public class FspTabuSearchFactory extends JspTabuSearchFactory {
+public class FspTabuSearchFactory
+    implements IAlgorithmFactory<JspPhenotype, JspTabuSearchSolution> {
+
+  @Override
+  public Class<?> getAlgorithmClass() {
+    return TabuSearchAdapter.class;
+  }
+
+  @Override
+  public IAlgorithmAdapter<JspPhenotype, JspTabuSearchSolution> createAlgorithm(
+      ProblemInstance instance, IPhenotypeEvaluator<JspPhenotype> phenotypeEvaluator)
+      throws Exception {
+    return new JspTabuSearchAdapter(
+        new FspMoveManager((JspJobsDefinition) instance),
+        new FspObjectiveFunction((FspPhenotypeEvaluator) phenotypeEvaluator), phenotypeEvaluator);
+  }
 }
