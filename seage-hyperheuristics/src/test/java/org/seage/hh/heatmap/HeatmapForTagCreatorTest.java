b4f6fbfc457aa687c3fe2d0efea6950da10aa419
Richard Malek <rick@rickq.net>
2023-03-04 12:42:06 +0100

    #102 Added a basic test

--- seage-hyperheuristics/src/test/java/org/seage/hh/heatmap/HeatmapForTagCreatorTest.java
+++ seage-hyperheuristics/src/test/java/org/seage/hh/heatmap/HeatmapForTagCreatorTest.java
@@ -1,5 +1,45 @@
 package org.seage.hh.heatmap;
 
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.seage.hh.experimenter.ExperimentScoreCard;
+
 public class HeatmapForTagCreatorTest {
-  //todo
+  // todo
+  String sc1 = """
+      {
+        "algorithmName": "TabuSearch",
+        "totalScore": 0.7323786081897516,
+        "scorePerInstance": {
+          "FSP": {
+            "tai500_20_01": 0.7323786081897516
+          }
+        },
+        "scorePerProblem": {
+          "FSP": 0.7323786081897516
+        }
+      }
+      """;
+  String sc2 = """
+      {
+        "algorithmName": "TabuSearch",
+        "totalScore": 0.7323786081897516,
+        "scorePerInstance": {
+          "FSP": {
+            "tai500_20_01": 0.7323786081897516
+          }
+        },
+        "scorePerProblem": {
+          "FSP": 0.7323786081897516
+        }
+      }
+      """;
+
+  @Test
+  void testSC1() {
+    List<ExperimentScoreCard> scoreCards1 = HeatmapForTagCreator.parseScoreCardsJson(sc1);
+    List<ExperimentScoreCard> scoreCards2 = HeatmapForTagCreator.parseScoreCardsJson(sc2);
+    scoreCards1.addAll(scoreCards2);
+    List<ExperimentScoreCard> table = HeatmapForTagCreator.mergeScoreCards(scoreCards1);
+  }
 }
