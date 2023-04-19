0cf53e69bbae60e8944da5155986bac57387c41e
David Omrai <david.omrai@seznam.cz>
2023-04-19 19:38:19 +0200

    #107 Fixing the eslint warnings

--- seage-aal/src/test/java/org/seage/aal/algorithm/tabusearch/TestMoveManager.java
+++ seage-aal/src/test/java/org/seage/aal/algorithm/tabusearch/TestMoveManager.java
@@ -1,11 +1,13 @@
 package org.seage.aal.algorithm.tabusearch;
 
 import java.util.Random;
-
 import org.seage.metaheuristic.tabusearch.Move;
 import org.seage.metaheuristic.tabusearch.MoveManager;
 import org.seage.metaheuristic.tabusearch.Solution;
 
+/**
+ * .
+ */
 public class TestMoveManager implements MoveManager {
   private Random rnd = new Random(1);
 
@@ -19,7 +21,7 @@ public class TestMoveManager implements MoveManager {
         Integer o = s.solution[0];
         s.solution[0] = s.solution[ix];
         s.solution[ix] = o;
-      }
+      } 
     };
     return new Move[] { m, m };
   }
