b9c93f8187700c36ab4ced29f3c086bbb1b0a1e0
David Omrai <david.omrai@seznam.cz>
2023-06-15 21:53:53 +0200

    #104 Resolved few eslint warnings

--- seage-problems/discrete/sat/src/main/java/org/seage/problem/sat/antcolony/SatAnt.java
+++ seage-problems/discrete/sat/src/main/java/org/seage/problem/sat/antcolony/SatAnt.java
@@ -1,29 +1,26 @@
 /*******************************************************************************
  * Copyright (c) 2009 Richard Malek and SEAGE contributors
- * 
+
  * This file is part of SEAGE.
- * 
+
  * SEAGE is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- * 
+
  * SEAGE is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
+
  * You should have received a copy of the GNU General Public License along with
  * SEAGE. If not, @see <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.
- *
- */
 
-/**
  * Contributors: Richard Malek - Initial implementation
+ *               David Omrai - Revisiting and fixing the code
  */
 
 package org.seage.problem.sat.antcolony;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import org.seage.metaheuristic.antcolony.Ant;
@@ -35,6 +32,7 @@ import org.seage.problem.sat.FormulaEvaluator;
 
 /**
  * SatAntBrain class.
+ *
  * @author Zagy
  */
 public class SatAnt extends Ant {
@@ -44,6 +42,7 @@ public class SatAnt extends Ant {
 
   /**
    * .
+   *
    * @param initNodePath .
    * @param formula .
    * @param formulaEvaluator .
