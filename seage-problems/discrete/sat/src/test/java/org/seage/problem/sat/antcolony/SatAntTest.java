b2404ca6954c031bd6ee486eef969fd35e5ed1aa
David Omrai <david.omrai@seznam.cz>
2023-06-15 10:05:58 +0200

    #104 Created and prepared two tests for SatAnt

---
+++ seage-problems/discrete/sat/src/test/java/org/seage/problem/sat/antcolony/SatAntTest.java
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Richard Malek and SEAGE contributors
+
+ * This file is part of SEAGE.
+
+ * SEAGE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * SEAGE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with SEAGE. If not, @see <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.
+
+ * Contributors:
+ *     Richard Malek
+ *     - Initial implementation
+ *     David Omrai
+ *     - Implementation of tests
+ */
+
+package org.seage.problem.sat.antcolony;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Class tests the method of SatAnt.
+ */
+public class SatAntTest {
+  @Test
+  public void testGetAvailableNodes() {
+    assertTrue(false);
+    // TODO
+  }
+
+  @Test
+  public void testGetNodeDistance() {
+    assertTrue(false);
+    // TODO
+  }
+}
