677f0f72498bc6aec68805eb9d086fc36c5f2c1a
David Omrai <david.omrai@seznam.cz>
2023-06-16 11:18:09 +0200

    #104 Bug fix

--- seage-problems/discrete/sat/src/main/java/org/seage/problem/sat/antcolony/SatAntColonyFactory.java
+++ seage-problems/discrete/sat/src/main/java/org/seage/problem/sat/antcolony/SatAntColonyFactory.java
@@ -62,8 +62,8 @@ public class SatAntColonyFactory implements IAlgorithmFactory<SatPhenotype, Ant>
 
       @Override
       public SatPhenotype solutionToPhenotype(Ant solution) throws Exception {        
-        SatPhenotype result = new SatPhenotype(new Boolean[solution.getNodeIDsAlongPath().size()]);
-        for (int i = 0; i < result.getSolution().length; i++) {
+        SatPhenotype result = new SatPhenotype(new Boolean[formula.getLiteralCount()]);
+        for (int i = 0; i < solution.getNodeIDsAlongPath().size(); i++) {
           Integer value = (Integer) solution.getNodeIDsAlongPath().get(i);
           result.getSolution()[i] = value > 0;
         }
