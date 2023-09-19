a6f487b7fe434c58f0a8798690ce83e53346f107
David Omrai <david.omrai@seznam.cz>
2023-09-19 10:47:24 +0200

    #114 Fixed Rosenbrock ff and ga

--- seage-problems/continuous/rosenbrock/src/test/java/org/seage/problem/rosenbrock/fireflies/RosenbrockFirefliesTest.java
+++ seage-problems/continuous/rosenbrock/src/test/java/org/seage/problem/rosenbrock/fireflies/RosenbrockFirefliesTest.java
@@ -10,7 +10,7 @@
 
  * SEAGE is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
 
  * You should have received a copy of the GNU General Public License
@@ -20,70 +20,74 @@
 
 /**
  * Contributors:
- *     Jan Zmatlik
- *     - Initial implementation
+ *   Jan Zmatlik
+ *   - Initial implementation
  */
 
 package org.seage.problem.rosenbrock.fireflies;
 
+import java.util.Arrays;
+import java.util.List;
 import org.seage.metaheuristic.fireflies.FireflyOperator;
 import org.seage.metaheuristic.fireflies.FireflySearch;
 import org.seage.metaheuristic.fireflies.Solution;
 
 /**
+ * .
  *
  * @author Jan Zmatlik
  */
-public class RosenbrockFirefliesTest
-{
-    public static void main(String[] args) throws Exception
-    {
-    	int dimension =4;
-        boolean _withDecreasingRandomness=false;
-        boolean _withHillClimbingBestSolution=false;
-        boolean _bestSolutionNoMove=false;
-        double _initialIntensity=1;
-        double _initialRandomness=5;
-        double _finalRandomness=2;
-        double _absorption=0.003;//0.025;
-        double _timeStep=0.7;//0.15;
-        int populationSize = 100;
-        boolean _maximizing = false;
-        int iterationsToGo = 1000;
-        double minBound = -10;
-        double maxBound = 10;
+public class RosenbrockFirefliesTest {
+  public static void main(String[] args) throws Exception {
+    int dimension = 4;
+    boolean withDecreasingRandomness = false;
+    boolean withHillClimbingBestSolution = false;
+    boolean bestSolutionNoMove = false;
+    double initialIntensity = 1;
+    double initialRandomness = 5;
+    double finalRandomness = 2;
+    double absorption = 0.003;//0.025;
+    double timeStep = 0.7;//0.15;
+    int populationSize = 100;
+    boolean maximizing = false;
+    int iterationsToGo = 1000;
+    double minBound = -10;
+    double maxBound = 10;
 
-        FireflyOperator fo = new ContinuousFireflyOperator(_initialIntensity,_initialRandomness,_finalRandomness,_absorption,_timeStep,_withDecreasingRandomness,dimension,minBound,maxBound);
-        RosenbrockObjectiveFunction rof = new RosenbrockObjectiveFunction();
-                
-        FireflySearch fs = new FireflySearch(fo, rof);
-//        fs.addFireflySearchListener(this);
-        fs.setWithDecreasingRandomness(_withDecreasingRandomness);
-        fs.setWithHillClimbingBestSolution(_withHillClimbingBestSolution);
-        fs.setInitialIntensity(_initialIntensity);
-        fs.setInitialRandomness(_initialRandomness);
-        fs.setFinalRandomness(_finalRandomness);
-        fs.setBestSolutionNoMove(_bestSolutionNoMove);
-        fs.setAbsorption(_absorption);
-        fs.setTimeStep(_timeStep);
-        fs.setPopulationCount(populationSize);
-        fs.setMaximizing(_maximizing);
-        fs.setIterationsToGo(iterationsToGo);
-//        System.out.println("Length of solution"+(new QapRandomSolution(facilityLocations)._assign.length));
-        Solution[] solutions = generateInitialSolutions(populationSize,fo);
-        fs.startSolving(solutions);   
-        System.out.println(fs.getBestSolution().getObjectiveValue()[0]);
-        for(int i = 0; i < dimension; i++)
-            System.out.print(" " + ((ContinuousSolution)fs.getBestSolution()).getAssign()[i]);
+    FireflyOperator fo = new ContinuousFireflyOperator(
+        initialIntensity, initialRandomness, finalRandomness, absorption, 
+        timeStep, withDecreasingRandomness, dimension, minBound, maxBound);
+    RosenbrockObjectiveFunction rof = new RosenbrockObjectiveFunction();
+        
+    FireflySearch fs = new FireflySearch(fo, rof);
+    // fs.addFireflySearchListener(this);
+    fs.setWithDecreasingRandomness(withDecreasingRandomness);
+    fs.setWithHillClimbingBestSolution(withHillClimbingBestSolution);
+    fs.setInitialIntensity(initialIntensity);
+    fs.setInitialRandomness(initialRandomness);
+    fs.setFinalRandomness(finalRandomness);
+    fs.setBestSolutionNoMove(bestSolutionNoMove);
+    fs.setAbsorption(absorption);
+    fs.setTimeStep(timeStep);
+    fs.setPopulationCount(populationSize);
+    fs.setMaximizing(maximizing);
+    fs.setIterationsToGo(iterationsToGo);
+    // System.out.println("Length of solution"+(new QapRandomSolution(facilityLocations).assign.length));
+    List<Solution> solutions = Arrays.asList(generateInitialSolutions(populationSize, fo));
+    fs.startSolving(solutions);  
+    System.out.println(fs.getBestSolution().getObjectiveValue()[0]);
+    for (int i = 0; i < dimension; i++) {
+      System.out.print(" " + ((ContinuousSolution) fs.getBestSolution()).getAssign()[i]);
     }
+  }
 
-    private static Solution[] generateInitialSolutions(int count, FireflyOperator fo) throws Exception
-    {
-        Solution[] result = new Solution[count];
-//        result[0]=new QapGreedyStartSolution(fl);
-        for(int i=0;i<count;i++){
-            result[i]= fo.randomSolution();
-        }
-        return result;
+  private static Solution[] generateInitialSolutions(
+      int count, FireflyOperator fo) throws Exception {
+    Solution[] result = new Solution[count];
+    // result[0]=new QapGreedyStartSolution(fl);
+    for (int i = 0; i < count; i++) {
+      result[i] = fo.randomSolution();
     }
+    return result;
+  }
 }
--- seage-problems/continuous/rosenbrock/src/test/java/org/seage/problem/rosenbrock/genetics/RosenbrockGeneticAlgorithmTest.java
+++ seage-problems/continuous/rosenbrock/src/test/java/org/seage/problem/rosenbrock/genetics/RosenbrockGeneticAlgorithmTest.java
@@ -20,97 +20,108 @@
 
 package org.seage.problem.rosenbrock.genetics;
 
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 import java.util.ArrayList;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.seage.aal.algorithm.AlgorithmAdapterImpl;
 import org.seage.metaheuristic.IAlgorithmListener;
 import org.seage.metaheuristic.genetics.ContinuousGeneticOperator;
 import org.seage.metaheuristic.genetics.GeneticAlgorithm;
 import org.seage.metaheuristic.genetics.GeneticAlgorithmEvent;
 import org.seage.metaheuristic.genetics.Subject;
 import org.seage.problem.rosenbrock.RosenbrockFunction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Rosenbrock genetic algorithm test.
  */
 public class RosenbrockGeneticAlgorithmTest {
-	@Test
-	void testRosenbrockGeneticAlgorithm() throws Exception {
-		try {
-			int dim = 11;
+  private static final Logger logger = 
+      LoggerFactory.getLogger(AlgorithmAdapterImpl.class.getName());
+  @Test
+  void testRosenbrockGeneticAlgorithm() throws Exception {
+    try {
+      int dim = 11;
 
-			double test[] = new double[dim];
-			for (int i = 0; i < dim; i++)
-				test[i] = 1;
-			System.out.println(RosenbrockFunction.f(test));
+      double[] test = new double[dim];
+      for (int i = 0; i < dim; i++) {
+        test[i] = 1;
+      }
+      System.out.println(RosenbrockFunction.f(test));
 
-			ContinuousGeneticOperator.Limit[] limits = new ContinuousGeneticOperator.Limit[dim];
-			ContinuousGeneticOperator<Subject<Double>> operator = new ContinuousGeneticOperator<Subject<Double>>(limits);
+      ContinuousGeneticOperator.Limit[] limits = new ContinuousGeneticOperator.Limit[dim];
+      ContinuousGeneticOperator<Subject<Double>> operator =
+          new ContinuousGeneticOperator<Subject<Double>>(limits);
 
-			for (int i = 0; i < dim; i++)
-				limits[i] = new  ContinuousGeneticOperator.Limit(-10, 10);
+      for (int i = 0; i < dim; i++) {
+        limits[i] = new ContinuousGeneticOperator.Limit(-10, 10);
+      }
 
-			GeneticAlgorithm<Subject<Double> > gs = new GeneticAlgorithm<Subject<Double> >(operator, new RosenbrockEvaluator());
-			gs.addGeneticSearchListener(new IAlgorithmListener<GeneticAlgorithmEvent<Subject<Double> >>() {
-				
-				@Override
-				public void noChangeInValueIterationMade(GeneticAlgorithmEvent<Subject<Double> > e) {
-					// TODO Auto-generated method stub
-					
-				}
-				
-				@Override
-				public void newBestSolutionFound(GeneticAlgorithmEvent<Subject<Double> > e) {
-					System.out.println(e.getGeneticSearch().getBestSubject().getObjectiveValue()[0]);
-					
-				}
-				
-				@Override
-				public void iterationPerformed(GeneticAlgorithmEvent<Subject<Double> > e) {
-					// TODO Auto-generated method stub
-					
-				}
-				
-				@Override
-				public void algorithmStopped(GeneticAlgorithmEvent<Subject<Double> > e) {
-					// TODO Auto-generated method stub
-					
-				}
-				
-				@Override
-				public void algorithmStarted(GeneticAlgorithmEvent<Subject<Double> > e) {
-					// TODO Auto-generated method stub
-					
-				}
-			});
-			gs.setCrossLengthPct(40);
-			gs.setEliteSubjectsPct(10);
-			gs.setIterationToGo(10000);
-			gs.setMutateChromosomeLengthPct(20);
-			gs.setMutatePopulationPct(10);			
-			gs.setPopulationCount(1000);
-			gs.setRandomSubjectsPct(1);
+      GeneticAlgorithm<Subject<Double>> gs =
+          new GeneticAlgorithm<Subject<Double>>(operator, new RosenbrockEvaluator());
+      gs.addGeneticSearchListener(new IAlgorithmListener<GeneticAlgorithmEvent<Subject<Double>>>() {
 
-			Double[] dimArray = new Double[dim];
-			for(int i=0;i<dim;i++)
-				dimArray[i] = new Double(0);
-			
-			ArrayList<Subject<Double>> subjects = new ArrayList<Subject<Double>>();
-			for (int i = 0; i < gs.getPopulationCount(); i++) {
-				subjects.add( new Subject<Double>(dimArray));
-				operator.randomize(subjects.get(i));
-			}
+        @Override
+        public void noChangeInValueIterationMade(GeneticAlgorithmEvent<Subject<Double>> e) {
+          // TODO Auto-generated method stub
+        }
 
-			gs.startSearching(subjects);
+        @Override
+        public void newBestSolutionFound(GeneticAlgorithmEvent<Subject<Double>> e) {
+          System.out.println(e.getGeneticSearch().getBestSubject().getObjectiveValue()[0]);
 
-			System.out.println(gs.getBestSubject().getObjectiveValue()[0]);
-			for (int i = 0; i < dim; i++)
-				System.out.print(gs.getBestSubject().getChromosome().getGene(i) + " ");
-			System.out.println();
-		}
-		catch (Exception e) {
-			// TODO Auto-generated catch block
-			log.error("{}", ex.getMessage(), ex);
-		}
-	}
+        }
+
+        @Override
+        public void iterationPerformed(GeneticAlgorithmEvent<Subject<Double>> e) {
+          // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void algorithmStopped(GeneticAlgorithmEvent<Subject<Double>> e) {
+          // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void algorithmStarted(GeneticAlgorithmEvent<Subject<Double>> e) {
+          // TODO Auto-generated method stub
+        }
+      });
+      gs.setCrossLengthPct(40);
+      gs.setEliteSubjectsPct(10);
+      gs.setIterationToGo(10000);
+      gs.setMutateChromosomeLengthPct(20);
+      gs.setMutatePopulationPct(10);
+      gs.setPopulationCount(1000);
+      gs.setRandomSubjectsPct(1);
+
+      Double[] dimArray = new Double[dim];
+      for (int i = 0; i < dim; i++) {
+        dimArray[i] = 0.0;
+      }
+
+      ArrayList<Subject<Double>> subjects = new ArrayList<Subject<Double>>();
+      for (int i = 0; i < gs.getPopulationCount(); i++) {
+        subjects.add(new Subject<Double>(dimArray));
+        operator.randomize(subjects.get(i));
+      }
+
+      gs.startSearching(subjects);
+
+      System.out.println(gs.getBestSubject().getObjectiveValue()[0]);
+      for (int i = 0; i < dim; i++) {
+        System.out.print(gs.getBestSubject().getChromosome().getGene(i) + " ");
+      }
+      System.out.println();
+      assertNull(null);
+    }
+    catch (Exception ex) {
+      // TODO Auto-generated catch block
+      logger.error("{}", ex.getMessage(), ex);
+    }
+  }
 }
