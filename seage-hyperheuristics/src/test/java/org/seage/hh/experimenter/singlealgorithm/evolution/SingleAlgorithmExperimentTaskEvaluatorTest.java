2340a552e2981aab9f340c77273c6e24e74eb47f
David Omrai <david.omrai@seznam.cz>
2023-09-29 12:28:12 +0200

    #113 Created first part of test, initializing of all the necessary elements

--- seage-hyperheuristics/src/test/java/org/seage/hh/experimenter/singlealgorithm/evolution/SingleAlgorithmExperimentTaskEvaluatorTest.java
+++ seage-hyperheuristics/src/test/java/org/seage/hh/experimenter/singlealgorithm/evolution/SingleAlgorithmExperimentTaskEvaluatorTest.java
@@ -4,23 +4,75 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import org.junit.jupiter.api.BeforeAll;
+import org.seage.aal.problem.ProblemConfig;
 import org.seage.aal.problem.ProblemInfo;
 import org.seage.aal.problem.ProblemInstanceInfo;
 import org.seage.aal.problem.ProblemProvider;
+import org.seage.data.DataNode;
+import org.seage.hh.experimenter.configurator.FeedbackConfigurator;
 import org.seage.hh.knowledgebase.db.dbo.ExperimentTaskRecord;
 
 public class SingleAlgorithmExperimentTaskEvaluatorTest {
   SingleAlgorithmExperimentTaskEvaluator evaluator;
+  ProblemInfo problemInfo;
+  HashMap<String, ProblemInstanceInfo> instancesInfo;
+  List<String> instanceIDs;
+  String problemID;
+  FeedbackConfigurator feedbackConfigurator;
+  List<SingleAlgorithmExperimentTaskSubject> subjects;
+
 
   private Void reportFn(ExperimentTaskRecord experiment) {
     // todo
     return null;
   }
 
+  List<SingleAlgorithmExperimentTaskSubject> initSubjects(
+      ProblemInfo problemInfo, List<String>instanceslList, 
+      String algorithmID, int numOfSubjects) throws Exception {
+    List<SingleAlgorithmExperimentTaskSubject> result = new ArrayList<>();
+    int numPerInstance = Math.max(
+        instanceIDs.size(), 
+        (int) Math.ceil(numOfSubjects / (double) instanceIDs.size()));
+    int curNumOfSubjects = 0;
+
+    for (String instanceID : instanceIDs) {
+      if (curNumOfSubjects >= numOfSubjects) {
+        break;
+      }
+
+      ProblemConfig[] pc = feedbackConfigurator.prepareConfigs(
+        problemInfo, instanceID, algorithmID, numPerInstance);
+
+      List<DataNode> params = problemInfo.getDataNode("Algorithms").getDataNodeById(algorithmID)
+          .getDataNodes("Parameter");
+
+      for (int i = 0; i < numPerInstance; i++) {
+        if (curNumOfSubjects >= numOfSubjects) {
+          break;
+        }
+        
+        String[] names = new String[params.size()];
+        Double[] values = new Double[params.size()];
+        for (int j = 0; j < params.size(); j++) {
+          names[j] = params.get(j).getValueStr("name");
+          values[j] = pc[i].getDataNode(
+            "Algorithm").getDataNode("Parameters").getValueDouble(names[j]);
+        }
+        result.add(new SingleAlgorithmExperimentTaskSubject(names, values));
+
+        curNumOfSubjects += 1;
+      }
+    }
+
+    // Return the right size of results
+    return result.subList(0, numOfSubjects);
+  }
+
   @BeforeAll
   void setEvaluator() throws Exception {
-    String problemID = "TSP";
-    List<String> instanceIDs = new ArrayList<>() {
+    this.problemID = "TSP";
+    this.instanceIDs = new ArrayList<>() {
       {
         add("berlin52");
         add("eil51");
@@ -44,5 +96,15 @@ public class SingleAlgorithmExperimentTaskEvaluatorTest {
       0, 
       instancesInfo, 
       this::reportFn);
+    this.problemInfo = 
+      ProblemProvider.getProblemProviders().get(problemID).getProblemInfo();
+    this.instancesInfo = new HashMap<>();
+    for (String instanceID : instanceIDs) {
+      this.instancesInfo.put(instanceID, problemInfo.getProblemInstanceInfo(instanceID));
+    }
+
+    this.feedbackConfigurator = new FeedbackConfigurator(0.0);
+    // Initialize subjects
+    this.subjects = initSubjects(problemInfo, instanceIDs, problemID, 10);
   }
 }
