658618277e664152961bcf80f414b8ce1b61ee53
Richard Malek <rick@rickq.net>
2023-10-14 13:10:54 +0200

    #115 Fixed the test packages

--- seage-hyperheuristics/src/test/java/org/seage/hh/configurator/RandomConfiguratorTest.java
+++ seage-hyperheuristics/src/test/java/org/seage/hh/configurator/RandomConfiguratorTest.java
@@ -1,4 +1,4 @@
-package org.seage.hh.experiment.configurator;
+package org.seage.hh.configurator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
--- seage-hyperheuristics/src/test/java/org/seage/hh/heatmap/ExperimentScoreCardTest.java
+++ seage-hyperheuristics/src/test/java/org/seage/hh/heatmap/ExperimentScoreCardTest.java
@@ -1,4 +1,4 @@
-package org.seage.hh.experimenter2;
+package org.seage.hh.heatmap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
